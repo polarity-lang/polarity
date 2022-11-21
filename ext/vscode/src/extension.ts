7e6e1d41b0d730fe6d33b224832d9cbb62a80aa8
Tim Süberkrüb <dev@timsueberkrueb.io>
2022-11-21 14:55:28 +0100

    Add prettier to VSCode plugin

--- ext/vscode/src/extension.ts
+++ ext/vscode/src/extension.ts
@@ -1,54 +1,54 @@
-
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as vscodelang from 'vscode-languageclient';
+import * as vscode from "vscode";
+import * as vscodelang from "vscode-languageclient";
 
 let client: vscodelang.LanguageClient;
 
 // This method is called when the "xfn" extension is activated.
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "xfn" is now active!');
 
-	console.log('Congratulations, your extension "xfn" is now active!');
+  let config = vscode.workspace.getConfiguration("xfn");
 
-	let config = vscode.workspace.getConfiguration("xfn");
+  let defaultCmd = "xfunc";
+  let langCmd = config.get<string>("executable") || defaultCmd;
 
-	let defaultCmd = "xfunc";
-	let langCmd = config.get<string>("executable") || defaultCmd;
+  // Assemble arguments
+  let args: string[] = [];
+  args.push("lsp");
 
-    // Assemble arguments
-    let args: string[] = [];
-    args.push("lsp");
+  let serverOptions: vscodelang.ServerOptions = {
+    run: {
+      command: langCmd,
+      args: args,
+      options: {},
+    },
+    debug: {
+      command: langCmd,
+      args: args,
+      options: {},
+    },
+  };
 
-	let serverOptions: vscodelang.ServerOptions = {
-        run: {
-            command: langCmd,
-            args: args,
-            options: {}
-        },
-        debug: {
-            command: langCmd,
-            args: args,
-            options: {}
-        }
-    };
+  let clientOptions: vscodelang.LanguageClientOptions = {
+    documentSelector: [
+      {
+        scheme: "file",
+        language: "xfn",
+      },
+    ],
+    diagnosticCollectionName: "xfn",
+  };
 
-    let clientOptions: vscodelang.LanguageClientOptions = {
-        documentSelector: [{
-            scheme: 'file',
-            language: 'xfn'
-        }],
-        diagnosticCollectionName: "xfn"
-    };
+  client = new vscodelang.LanguageClient(
+    "xfnLanguageServer",
+    "XFN Language Server",
+    serverOptions,
+    clientOptions
+  );
 
-    client = new vscodelang.LanguageClient(
-        'xfnLanguageServer',
-        'XFN Language Server',
-        serverOptions,
-        clientOptions
-    );
-
-	context.subscriptions.push(client.start());
+  context.subscriptions.push(client.start());
 }
 
 // this method is called when your extension is deactivated
