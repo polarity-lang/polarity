65d225cad7331cbac80017ad0c8eb8ffa5199ff4
David Binder <david.binder@uni-tuebingen.de>
2023-03-31 17:04:16 +0200

    Somewhat fix error messages in LSP

--- util/lsp/src/diagnostics.rs
+++ util/lsp/src/diagnostics.rs
@@ -1,5 +1,6 @@
 use lsp_types::NumberOrString;
 use miette::Diagnostic;
+use miette::SourceSpan;
 use tower_lsp::lsp_types;
 
 use miette_util::FromMiette;
@@ -21,27 +22,40 @@ pub trait Diagnostics {
 
 impl Diagnostics for DatabaseView<'_> {
     fn error_diagnostics(&self, error: Error) -> Vec<lsp_types::Diagnostic> {
-        let labels = error.labels().unwrap_or_else(|| Box::new([].into_iter()));
+        // Compute the range where the error should be displayed.
+        // The range is computed from the first available label, otherwise
+        // the default range is used, which corresponds to the beginning of the
+        // file.
+        let span = get_span(&error);
+        let range = span
+            .and_then(|x| self.span_to_locations(x.from_miette()))
+            .map(ToLsp::to_lsp)
+            .unwrap_or_default();
 
-        labels
-            .filter_map(|label| {
-                let span = label.inner().from_miette();
-                let range = self.span_to_locations(span)?.to_lsp();
-                let message =
-                    label.label().map(ToOwned::to_owned).unwrap_or_else(|| error.to_string());
-                let diag = lsp_types::Diagnostic {
-                    range,
-                    message,
-                    severity: error.severity().map(ToLsp::to_lsp),
-                    code: error.code().map(|x| NumberOrString::String(format!("{}", x))),
-                    code_description: None,
-                    source: None,
-                    related_information: None,
-                    tags: None,
-                    data: None,
-                };
-                Some(diag)
-            })
-            .collect()
+        // Compute the message.
+        let message = error.to_string();
+
+        let diag = lsp_types::Diagnostic {
+            range,
+            message,
+            severity: error.severity().map(ToLsp::to_lsp),
+            code: error.code().map(|x| NumberOrString::String(format!("{}", x))),
+            code_description: None,
+            source: None,
+            related_information: None,
+            tags: None,
+            data: None,
+        };
+        vec![diag]
+    }
+}
+
+fn get_span<T: Diagnostic>(err: &T) -> Option<SourceSpan> {
+    match err.labels() {
+        Some(spans) => {
+            let x = spans.into_iter().next();
+            x.map(|y| *y.inner())
+        }
+        None => None,
     }
 }
