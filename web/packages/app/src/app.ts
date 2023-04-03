9786edb1750ad6b51f5f876aaf5984ccd6031184
David Binder <david.binder@uni-tuebingen.de>
2023-04-03 17:05:00 +0200

    Change theme

--- web/packages/app/src/app.ts
+++ web/packages/app/src/app.ts
@@ -89,6 +89,16 @@ export default class App {
       void event;
       void handleHashChange();
     });
+    
+    addEventListener("transitionstart", (event) => {
+      let thm = document.documentElement.getAttribute("data-theme");
+      if (thm == "dark") {
+        monaco.editor.setTheme('vs-dark')
+
+      } else if (thm == "light") {
+        monaco.editor.setTheme('vs-light')
+      }
+    })
 
     return model;
   }
