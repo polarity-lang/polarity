f38df1ae820b258be37417de20fda52b30afd62a
Tim Süberkrüb <dev@timsueberkrueb.io>
2023-04-12 13:18:47 +0200

    Fix issues pointed out by bash linter

--- scripts/oopsla_snippets.sh
+++ scripts/oopsla_snippets.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-IMPL_DIR=`pwd`
-TMP_DIR=`mktemp -d`
+IMPL_DIR=$(pwd)
+TMP_DIR=$(mktemp -d)
 function cleanup {
   rm -rf "$TMP_DIR"
 }
 trap cleanup EXIT
 
-cd $TMP_DIR
+cd "$TMP_DIR" || exit
 git clone --depth 1 git@github.com:ps-tuebingen/dependent-xfunctionalization.git paper-repo/
-rm $IMPL_DIR/oopsla_examples/*.xfn
-cp paper-repo/snippets/fullwidth/* $IMPL_DIR/oopsla_examples
-cp paper-repo/snippets/halfwidth/* $IMPL_DIR/oopsla_examples
+rm "$IMPL_DIR/oopsla_examples/*.xfn"
+cp paper-repo/snippets/fullwidth/* "$IMPL_DIR/oopsla_examples"
+cp paper-repo/snippets/halfwidth/* "$IMPL_DIR/oopsla_examples"
 # Strip @hidden attributes from examples
-find $IMPL_DIR/oopsla_examples -type f -name '*.xfn' -exec sed -i '/@hidden$/d' {} \;
-find $IMPL_DIR/oopsla_examples -type f -name '*.xfn' -exec sed -i 's/@hidden //g' {} \;
+find "$IMPL_DIR/oopsla_examples" -type f -name '*.xfn' -exec sed -i '/@hidden$/d' {} \;
+find "$IMPL_DIR/oopsla_examples" -type f -name '*.xfn' -exec sed -i 's/@hidden //g' {} \;
