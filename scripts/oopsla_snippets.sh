bee18e147946a63f73a8b926f8cf07dce6037e25
Tim Süberkrüb <dev@timsueberkrueb.io>
2023-04-15 08:25:33 +0200

    Whitelist OOPSLA snippets

--- scripts/oopsla_snippets.sh
+++ scripts/oopsla_snippets.sh
@@ -10,8 +10,14 @@ trap cleanup EXIT
 cd "$TMP_DIR" || exit
 git clone --depth 1 git@github.com:ps-tuebingen/dependent-xfunctionalization.git paper-repo/
 rm "$IMPL_DIR/oopsla_examples"/*.xfn
-cp paper-repo/snippets/fullwidth/* "$IMPL_DIR/oopsla_examples"
-cp paper-repo/snippets/halfwidth/* "$IMPL_DIR/oopsla_examples"
+mkdir "$TMP_DIR/staging"
+cp paper-repo/snippets/fullwidth/* "$TMP_DIR/staging"
+cp paper-repo/snippets/halfwidth/* "$TMP_DIR/staging"
+
+while read example; do
+  cp "$TMP_DIR/staging/$example" "$IMPL_DIR/oopsla_examples"
+done < "$IMPL_DIR/oopsla_examples/whitelist.txt"
+
 # Strip @hidden attributes from examples
 find "$IMPL_DIR/oopsla_examples" -type f -name '*.xfn' -exec sed -i '/@hidden$/d' {} \;
 find "$IMPL_DIR/oopsla_examples" -type f -name '*.xfn' -exec sed -i 's/@hidden //g' {} \;
