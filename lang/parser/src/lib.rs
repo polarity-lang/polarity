79cbf2f3684b40d0cd5310d8d5c74b608d2de82d
David Binder <david.binder@uni-tuebingen.de>
2023-06-21 18:18:28 +0200

    Remove cst.rs from parser

--- lang/parser/src/lib.rs
+++ lang/parser/src/lib.rs
@@ -1,6 +1,15 @@
-mod cst;
 mod grammar;
 mod result;
 
-pub use cst::*;
+use std::rc::Rc;
+
+use grammar::cst::{ExpParser, PrgParser};
 pub use result::*;
+
+pub fn parse_exp(s: &str) -> Result<Rc<syntax::cst::Exp>, ParseError> {
+    ExpParser::new().parse(s).map_err(From::from)
+}
+
+pub fn parse_program(s: &str) -> Result<syntax::cst::Prg, ParseError> {
+    PrgParser::new().parse(s).map_err(From::from)
+}
