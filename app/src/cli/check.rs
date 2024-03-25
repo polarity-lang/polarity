321276b8f278376eab84b1feaea2a2244971b1ad
David Binder <david.binder@uni-tuebingen.de>
2024-03-25 13:52:41 +0100

    Implement main expressions (#168)
    - `pol run` executes the main expression in a file
    - `pol check` only typechecks a file

---
+++ app/src/cli/check.rs
@@ -0,0 +1,20 @@
+use std::path::PathBuf;
+
+use query::{Database, File};
+
+use crate::result::IOError;
+
+#[derive(clap::Args)]
+pub struct Args {
+    #[clap(value_parser, value_name = "FILE")]
+    filepath: PathBuf,
+}
+
+pub fn exec(cmd: Args) -> miette::Result<()> {
+    let mut db = Database::default();
+    let file = File::read(&cmd.filepath).map_err(IOError::from).map_err(miette::Report::from)?;
+    let view = db.add(file).query();
+    let _ = view.tst().map_err(|err| view.pretty_error(err))?;
+    println!("{} typechecked successfully!", cmd.filepath.display());
+    Ok(())
+}
--- app/src/cli/run.rs
+++ app/src/cli/run.rs
@@ -1,5 +1,8 @@
 use std::path::PathBuf;
 
+use miette::Diagnostic;
+use thiserror::Error;
+
 use printer::{ColorChoice, PrintExt, StandardStream};
 use query::{Database, File};
 use syntax::nf;
@@ -16,10 +19,11 @@ pub fn exec(cmd: Args) -> miette::Result<()> {
     let mut db = Database::default();
     let file = File::read(&cmd.filepath).map_err(IOError::from).map_err(miette::Report::from)?;
     let view = db.add(file).query();
+
     let nf = view.run().map_err(|err| view.pretty_error(err))?;
-    println!("{} typechecked successfully!", cmd.filepath.display());
-    if let Some(nf) = nf {
-        print_nf(&nf)
+    match nf {
+        Some(nf) => print_nf(&nf),
+        None => return Err(miette::Report::from(MainNotFound {})),
     }
     Ok(())
 }
@@ -29,3 +33,8 @@ fn print_nf(nf: &nf::Nf) {
     nf.print_colored(&Default::default(), &mut stream).expect("Failed to print to stdout");
     println!();
 }
+
+#[derive(Error, Diagnostic, Debug)]
+#[error("Main expression was not found")]
+#[diagnostic(help("Main expressions must be called \"main\" and not take any arguments."))]
+pub struct MainNotFound {}
