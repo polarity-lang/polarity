3564be963f4dc9c0fe71be13b08e487ae46ad393
David Binder <david.binder@uni-tuebingen.de>
2024-03-25 14:50:22 +0100

    Merge UnifyError, TypeError and EvalError

--- lang/elaborator/src/normalizer/normalize.rs
+++ lang/elaborator/src/normalizer/normalize.rs
@@ -8,9 +8,9 @@ use crate::result::*;
 pub trait Normalize {
     type Nf;
 
-    fn normalize(&self, prg: &ust::Prg, env: &mut Env) -> Result<Self::Nf, EvalError>;
+    fn normalize(&self, prg: &ust::Prg, env: &mut Env) -> Result<Self::Nf, TypeError>;
 
-    fn normalize_in_empty_env(&self, prg: &ust::Prg) -> Result<Self::Nf, EvalError> {
+    fn normalize_in_empty_env(&self, prg: &ust::Prg) -> Result<Self::Nf, TypeError> {
         self.normalize(prg, &mut Env::empty())
     }
 }
@@ -22,7 +22,7 @@ where
 {
     type Nf = <<T as Eval>::Val as ReadBack>::Nf;
 
-    fn normalize(&self, prg: &ust::Prg, env: &mut Env) -> Result<Self::Nf, EvalError> {
+    fn normalize(&self, prg: &ust::Prg, env: &mut Env) -> Result<Self::Nf, TypeError> {
         let val = self.eval(prg, env)?;
         val.read_back(prg)
     }
