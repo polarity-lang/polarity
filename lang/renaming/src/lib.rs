8065b85035f4494290635495947ddc74e2403e54
David Binder <david.binder@uni-tuebingen.de>
2024-04-21 14:50:39 +0200

    Replace map (#179)
    * Replace map
    
    ---------
    
    Co-authored-by: Tim Süberkrüb <dev@timsueberkrueb.io>

--- lang/renaming/src/lib.rs
+++ lang/renaming/src/lib.rs
@@ -1,22 +1,6 @@
 mod ast;
 mod ctx;
-mod ust;
 mod util;
 
+pub use ast::Rename;
 pub use ctx::Ctx;
-
-pub trait Rename: Sized {
-    fn rename(self) -> Self {
-        let mut ctx = Ctx::empty();
-        self.rename_in_ctx(&mut ctx)
-    }
-    fn rename_in_ctx(self, ctx: &mut Ctx) -> Self;
-}
-
-pub trait RenameInfo {
-    fn rename_in_ctx(self, ctx: &mut Ctx) -> Self;
-}
-
-pub trait RenameTelescope {
-    fn rename_telescope<T, F: FnOnce(&mut Ctx, Self) -> T>(&self, ctx: &mut Ctx, f: F) -> T;
-}
--- lang/syntax/src/trees/generic/mod.rs
+++ lang/syntax/src/trees/generic/mod.rs
@@ -1,12 +1,8 @@
 mod def;
-mod fold;
 mod lookup;
 pub mod lookup_table;
-mod map;
 mod visit;
 
 pub use def::*;
-pub use fold::*;
 pub use lookup::*;
-pub use map::*;
 pub use visit::*;
