2dc29e99b9e3807890aed86d0148c3ae95d12ef9
David Binder <david.binder@uni-tuebingen.de>
2024-05-16 17:06:20 +0200

    Add cli tests

---
+++ app/tests/cli_test.rs
@@ -0,0 +1,20 @@
+use assert_cmd::Command;
+
+/// The name of the CLI binary
+const BINARY: &str = "pol";
+
+/// Check that "pol --version" works correctly
+#[test]
+fn version_command() {
+    let mut cmd = Command::cargo_bin(BINARY).unwrap();
+    let assert = cmd.arg("--version").assert();
+    assert.success().stdout("polarity 0.1.0\n");
+}
+
+/// Check that "pol check" works correctly
+#[test]
+fn check_command() {
+    let mut cmd = Command::cargo_bin(BINARY).unwrap();
+    let assert = cmd.args(vec!["check", "examples/absurd.pol"]).assert();
+    assert.success().stdout("Typechecks!");
+}
