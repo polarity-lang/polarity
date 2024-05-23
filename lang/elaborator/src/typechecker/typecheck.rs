9d695f176e91344bc7fa6c4c80a36887afc6e07f
David Binder <david.binder@uni-tuebingen.de>
2024-05-23 22:32:30 +0200

    Use askama template for nicer trace

--- lang/elaborator/src/typechecker/typecheck.rs
+++ lang/elaborator/src/typechecker/typecheck.rs
@@ -2,8 +2,10 @@
 
 use std::rc::Rc;
 
+use askama::Template;
 use codespan::Span;
 use log::trace;
+
 use printer::PrintToString;
 
 use crate::normalizer::env::ToEnv;
@@ -102,6 +104,14 @@ impl CheckInfer for Exp {
 //
 //
 
+#[derive(Template)]
+#[template(path = "var_rule.txt")]
+struct VarRule<'a> {
+    ctx: &'a str,
+    var: &'a str,
+    typ: &'a str,
+}
+
 impl CheckInfer for Variable {
     /// The *checking* rule for variables is:
     /// ```text
@@ -129,6 +139,18 @@ impl CheckInfer for Variable {
     fn infer(&self, _prg: &Module, ctx: &mut Ctx) -> Result<Self, TypeError> {
         let Variable { span, idx, name, .. } = self;
         let typ_nf = ctx.lookup(*idx);
+
+        trace!(
+            "{}",
+            VarRule {
+                ctx: &ctx.vars.print_to_string(None),
+                var: name,
+                typ: &typ_nf.print_to_string(None)
+            }
+            .render()
+            .unwrap()
+        );
+
         Ok(Variable { span: *span, idx: *idx, name: name.clone(), inferred_type: Some(typ_nf) })
     }
 }
