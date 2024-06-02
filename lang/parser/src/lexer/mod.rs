1d66dbce714e6007f115d341496edc2b6770a35c
David Binder <david.binder@uni-tuebingen.de>
2024-06-02 14:10:13 +0200

    Implement lexer for tokens

--- lang/parser/src/lexer/mod.rs
+++ lang/parser/src/lexer/mod.rs
@@ -1,10 +1,16 @@
 use std::fmt;
 
-use logos::Logos;
+use logos::{Logos, SpannedIter};
 use num_bigint::BigUint;
 
+#[derive(Default, Debug, Clone, PartialEq)]
+pub enum LexicalError {
+    #[default]
+    InvalidToken,
+}
+
 #[derive(Logos, Debug, PartialEq)]
-#[logos(skip r"\s*")] // Ignore this regex pattern between tokens
+#[logos(skip r"\s*", error = LexicalError)]
 pub enum Token {
     // Keywords
     //
@@ -100,5 +106,27 @@ pub enum Token {
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self)
+    }
+}
+
+pub type Spanned<Tok, Loc, Error> = Result<(Loc, Tok, Loc), Error>;
+
+pub struct Lexer<'input> {
+    // instead of an iterator over characters, we have a token iterator
+    token_stream: SpannedIter<'input, Token>,
+}
+
+impl<'input> Lexer<'input> {
+    pub fn new(input: &'input str) -> Self {
+        // the Token::lexer() method is provided by the Logos trait
+        Self { token_stream: Token::lexer(input).spanned() }
+    }
+}
+
+impl<'input> Iterator for Lexer<'input> {
+    type Item = Spanned<Token, usize, LexicalError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.token_stream.next().map(|(token, span)| Ok((span.start, token?, span.end)))
     }
 }
