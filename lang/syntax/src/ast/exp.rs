192bb8719dcda9a72b86fc1d7c66225563dae257
David Binder <david.binder@uni-tuebingen.de>
2024-06-24 14:37:29 +0200

    Add Arg to AST

--- lang/syntax/src/ast/exp.rs
+++ lang/syntax/src/ast/exp.rs
@@ -64,6 +64,66 @@ where
     }
 }
 
+// Arg
+//
+//
+
+/// Arguments in an argument list can either be unnamed or named.
+/// Example for named arguments: `f(x := 1, y := 2)`
+/// Example for unnamed arguments: `f(1, 2)``
+#[derive(Debug, Clone, Derivative)]
+#[derivative(Eq, PartialEq, Hash)]
+pub enum Arg {
+    UnnamedArg(Rc<Exp>),
+    NamedArg(Ident, Rc<Exp>),
+}
+
+impl HasSpan for Arg {
+    fn span(&self) -> Option<Span> {
+        match self {
+            Arg::UnnamedArg(e) => e.span(),
+            Arg::NamedArg(_, e) => e.span(),
+        }
+    }
+}
+
+impl Shift for Arg {
+    fn shift_in_range<R: ShiftRange>(&self, range: R, by: (isize, isize)) -> Self {
+        match self {
+            Arg::UnnamedArg(e) => Arg::UnnamedArg(e.shift_in_range(range, by)),
+            Arg::NamedArg(i, e) => Arg::NamedArg(i.clone(), e.shift_in_range(range, by)),
+        }
+    }
+}
+
+impl Occurs for Arg {
+    fn occurs(&self, ctx: &mut LevelCtx, lvl: Lvl) -> bool {
+        match self {
+            Arg::UnnamedArg(e) => e.occurs(ctx, lvl),
+            Arg::NamedArg(_, e) => e.occurs(ctx, lvl),
+        }
+    }
+}
+
+impl HasTypeInfo for Arg {
+    fn typ(&self) -> Option<Rc<Exp>> {
+        match self {
+            Arg::UnnamedArg(e) => e.typ(),
+            Arg::NamedArg(_, e) => e.typ(),
+        }
+    }
+}
+
+impl Substitutable for Arg {
+    type Result = Arg;
+    fn subst<S: Substitution>(&self, ctx: &mut LevelCtx, by: &S) -> Self::Result {
+        match self {
+            Arg::UnnamedArg(e) => Arg::UnnamedArg(e.subst(ctx, by)),
+            Arg::NamedArg(i, e) => Arg::NamedArg(i.clone(), e.subst(ctx, by)),
+        }
+    }
+}
+
 // Exp
 //
 //
