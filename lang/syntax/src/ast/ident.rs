e3e4757578d6ea97ac1dbfc51be0b5d51aca2701
David Binder <david.binder@uni-tuebingen.de>
2024-07-04 11:24:35 +0200

    Add docs for difference between levels and indizes

--- lang/syntax/src/ast/ident.rs
+++ lang/syntax/src/ast/ident.rs
@@ -12,12 +12,52 @@ pub struct MetaVar {
     pub id: u64,
 }
 
+// Difference between two-level deBruijn indizes and levels
+//
+// Suppose we have the following context with a variable `v` which
+// should point to the element `f` in the context.
+//
+// ```text
+//  [[a,b,c],[d,e],[f,g,h],[i]] ⊢ v
+//                  ^             ^
+//                  \-------------/
+// ```
+//
+// There are two ways to achieve this. We can either count in the context from the
+// right, this is called deBruijn indizes, or we can count from the left, this is called
+// deBruin levels. Indizes look like this:
+//
+// ```text
+//  snd:                2 1 0
+//      [[a,b,c],[d,e],[f,g,h],[i]] ⊢ Idx { fst: 1, snd: 2}
+//        ^^^^^   ^^^   ^^^^^   ^
+//  fst:    3      2      1     0
+// ```
+// and levels look like this:
+// ```text
+//  snd:                0 1 2
+//      [[a,b,c],[d,e],[f,g,h],[i]] ⊢ Lvl { fst: 2, snd: 0}
+//        ^^^^^   ^^^   ^^^^^   ^
+//  fst     0      1      2     3
+// ```
+//
+// We use levels when we want to weaken the context, because the binding structure
+// remains intact when we add new binders `[j,k,l]` to the right of the context:
+// ```text
+//  snd:                0 1 2
+//      [[a,b,c],[d,e],[f,g,h],[i],[j,k,l]] ⊢ Lvl { fst: 2, snd: 0}
+//        ^^^^^   ^^^   ^^^^^   ^   ^^^^^
+//  fst     0      1      2     3     4
+// ```
+// We didn't have to change the Level, and it still refers to the same element of the context.
+
 /// Two-dimensional De-Bruijn index
 ///
 /// The first component counts the number of binder lists in scope between the variable
 /// and the binder list it originated from.
 /// The second component counts the number of binders in that binder list between the end
 /// of the binder list and the binder this variable originated from.
+///
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Idx {
     pub fst: usize,
--- lang/syntax/src/ast/traits/has_type.rs
+++ lang/syntax/src/ast/traits/has_type.rs
@@ -5,7 +5,7 @@ use crate::ast::exp::Exp;
 /// Trait for expressions which have a type.
 ///
 /// The function  `typ()` itself should not perform any non-trivial computation.
-/// You should first run evaluation on an expression before you call `typ()` on it,
+/// You should first run elaboration on an expression before you call `typ()` on it,
 /// otherwise the function is not guaranteed to return a result.
 pub trait HasType {
     /// Return the type of the expression.
