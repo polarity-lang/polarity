d1e85cea1af2c6d0552de243bceacb6ddbf55041
David Binder <david.binder@uni-tuebingen.de>
2024-09-15 20:11:38 +0200

    Factor out the Named trait

--- lang/ast/src/traits/mod.rs
+++ lang/ast/src/traits/mod.rs
@@ -1,11 +1,13 @@
 mod has_span;
 mod has_type;
+pub mod named;
 mod occurs;
 mod shift;
 pub mod subst;
 
 pub use has_span::*;
 pub use has_type::*;
+pub use named::*;
 pub use occurs::*;
 pub use shift::*;
 pub use subst::*;
