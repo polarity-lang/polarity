9cdb5a4b308037ddfae5acfd85d7de7ff186766f
David Binder <david.binder@uni-tuebingen.de>
2024-09-26 23:38:15 +0200

    Apply suggestion from review

--- lang/elaborator/src/lib.rs
+++ lang/elaborator/src/lib.rs
@@ -3,4 +3,6 @@ pub mod result;
 pub mod typechecker;
 pub mod unifier;
 
+pub use typechecker::type_info_table::build::build_type_info_table;
 pub use typechecker::type_info_table::ModuleTypeInfoTable;
+pub use typechecker::type_info_table::TypeInfoTable;
