5408c9b62103881136d3341853d48b5663fab644
David Binder <david.binder@uni-tuebingen.de>
2024-11-06 13:00:53 +0100

    Factor out TypCtor

--- lang/ast/src/exp/mod.rs
+++ lang/ast/src/exp/mod.rs
@@ -6,7 +6,7 @@ use derivative::Derivative;
 use pretty::DocAllocator;
 use printer::theme::ThemeExt;
 use printer::tokens::{
-    ABSURD, ARROW, AS, COLON, COLONEQ, COMATCH, COMMA, DOT, FAT_ARROW, MATCH, QUESTION_MARK, TYPE,
+    ABSURD, AS, COLON, COLONEQ, COMATCH, COMMA, DOT, FAT_ARROW, MATCH, QUESTION_MARK, TYPE,
     UNDERSCORE,
 };
 use printer::util::{BackslashExt, BracesExt};
@@ -22,6 +22,9 @@ use super::traits::HasSpan;
 use super::traits::Occurs;
 use super::HasType;
 use super::{ident::*, Shift, ShiftRange, ShiftRangeExt};
+
+mod typ_ctor;
+pub use typ_ctor::*;
 
 // Prints "{ }"
 pub fn empty_braces<'a>(alloc: &'a Alloc<'a>) -> Builder<'a> {
@@ -434,118 +437,6 @@ impl ContainsMetaVars for Variable {
         let Variable { span: _, idx: _, name: _, inferred_type } = self;
 
         inferred_type.contains_metavars()
-    }
-}
-
-// TypCtor
-//
-//
-
-/// A type constructor applied to arguments. The type of `TypCtor`
-/// is always the type universe `Type`.
-/// Examples: `Nat`, `List(Nat)`
-#[derive(Debug, Clone, Derivative)]
-#[derivative(Eq, PartialEq, Hash)]
-pub struct TypCtor {
-    /// Source code location
-    #[derivative(PartialEq = "ignore", Hash = "ignore")]
-    pub span: Option<Span>,
-    /// Name of the type constructor
-    pub name: Ident,
-    /// Arguments to the type constructor
-    pub args: Args,
-}
-
-impl TypCtor {
-    pub fn to_exp(&self) -> Exp {
-        Exp::TypCtor(self.clone())
-    }
-
-    /// A type application is simple if the list of arguments is empty.
-    pub fn is_simple(&self) -> bool {
-        self.args.is_empty()
-    }
-}
-
-impl HasSpan for TypCtor {
-    fn span(&self) -> Option<Span> {
-        self.span
-    }
-}
-
-impl From<TypCtor> for Exp {
-    fn from(val: TypCtor) -> Self {
-        Exp::TypCtor(val)
-    }
-}
-
-impl Shift for TypCtor {
-    fn shift_in_range<R: ShiftRange>(&mut self, range: &R, by: (isize, isize)) {
-        self.args.shift_in_range(range, by);
-    }
-}
-
-impl Occurs for TypCtor {
-    fn occurs(&self, ctx: &mut LevelCtx, lvl: Lvl) -> bool {
-        let TypCtor { args, .. } = self;
-        args.args.iter().any(|arg| arg.occurs(ctx, lvl))
-    }
-}
-
-impl HasType for TypCtor {
-    fn typ(&self) -> Option<Box<Exp>> {
-        Some(Box::new(TypeUniv::new().into()))
-    }
-}
-
-impl Substitutable for TypCtor {
-    type Result = TypCtor;
-    fn subst<S: Substitution>(&self, ctx: &mut LevelCtx, by: &S) -> Self {
-        let TypCtor { span, name, args } = self;
-        TypCtor { span: *span, name: name.clone(), args: args.subst(ctx, by) }
-    }
-}
-
-impl Print for TypCtor {
-    fn print_prec<'a>(
-        &'a self,
-        cfg: &PrintCfg,
-        alloc: &'a Alloc<'a>,
-        prec: Precedence,
-    ) -> Builder<'a> {
-        let TypCtor { span: _, name, args } = self;
-        if name.id == "Fun" && args.len() == 2 && cfg.print_function_sugar {
-            let arg = args.args[0].print_prec(cfg, alloc, 1);
-            let res = args.args[1].print_prec(cfg, alloc, 0);
-            let fun = arg.append(alloc.space()).append(ARROW).append(alloc.space()).append(res);
-            if prec == 0 {
-                fun
-            } else {
-                fun.parens()
-            }
-        } else {
-            let psubst = if args.is_empty() { alloc.nil() } else { args.print(cfg, alloc) };
-            alloc.typ(&name.id).append(psubst)
-        }
-    }
-}
-
-/// Implement Zonk for TypCtor
-impl Zonk for TypCtor {
-    fn zonk(
-        &mut self,
-        meta_vars: &crate::HashMap<MetaVar, crate::MetaVarState>,
-    ) -> Result<(), ZonkError> {
-        let TypCtor { span: _, name: _, args } = self;
-        args.zonk(meta_vars)
-    }
-}
-
-impl ContainsMetaVars for TypCtor {
-    fn contains_metavars(&self) -> bool {
-        let TypCtor { span: _, name: _, args } = self;
-
-        args.contains_metavars()
     }
 }
 
---
+++ lang/ast/src/exp/typ_ctor.rs
@@ -0,0 +1,119 @@
+use codespan::Span;
+use derivative::Derivative;
+use pretty::DocAllocator;
+use printer::{theme::ThemeExt, tokens::ARROW, Alloc, Builder, Precedence, Print, PrintCfg};
+
+use crate::{
+    ctx::LevelCtx, ContainsMetaVars, HasSpan, HasType, Occurs, Shift, ShiftRange, Substitutable,
+    Substitution, Zonk, ZonkError,
+};
+
+use super::{Args, Exp, Ident, Lvl, MetaVar, TypeUniv};
+
+/// A type constructor applied to arguments. The type of `TypCtor`
+/// is always the type universe `Type`.
+/// Examples: `Nat`, `List(Nat)`
+#[derive(Debug, Clone, Derivative)]
+#[derivative(Eq, PartialEq, Hash)]
+pub struct TypCtor {
+    /// Source code location
+    #[derivative(PartialEq = "ignore", Hash = "ignore")]
+    pub span: Option<Span>,
+    /// Name of the type constructor
+    pub name: Ident,
+    /// Arguments to the type constructor
+    pub args: Args,
+}
+
+impl TypCtor {
+    pub fn to_exp(&self) -> Exp {
+        Exp::TypCtor(self.clone())
+    }
+
+    /// A type application is simple if the list of arguments is empty.
+    pub fn is_simple(&self) -> bool {
+        self.args.is_empty()
+    }
+}
+
+impl HasSpan for TypCtor {
+    fn span(&self) -> Option<Span> {
+        self.span
+    }
+}
+
+impl From<TypCtor> for Exp {
+    fn from(val: TypCtor) -> Self {
+        Exp::TypCtor(val)
+    }
+}
+
+impl Shift for TypCtor {
+    fn shift_in_range<R: ShiftRange>(&mut self, range: &R, by: (isize, isize)) {
+        self.args.shift_in_range(range, by);
+    }
+}
+
+impl Occurs for TypCtor {
+    fn occurs(&self, ctx: &mut LevelCtx, lvl: Lvl) -> bool {
+        let TypCtor { args, .. } = self;
+        args.args.iter().any(|arg| arg.occurs(ctx, lvl))
+    }
+}
+
+impl HasType for TypCtor {
+    fn typ(&self) -> Option<Box<Exp>> {
+        Some(Box::new(TypeUniv::new().into()))
+    }
+}
+
+impl Substitutable for TypCtor {
+    type Result = TypCtor;
+    fn subst<S: Substitution>(&self, ctx: &mut LevelCtx, by: &S) -> Self {
+        let TypCtor { span, name, args } = self;
+        TypCtor { span: *span, name: name.clone(), args: args.subst(ctx, by) }
+    }
+}
+
+impl Print for TypCtor {
+    fn print_prec<'a>(
+        &'a self,
+        cfg: &PrintCfg,
+        alloc: &'a Alloc<'a>,
+        prec: Precedence,
+    ) -> Builder<'a> {
+        let TypCtor { span: _, name, args } = self;
+        if name.id == "Fun" && args.len() == 2 && cfg.print_function_sugar {
+            let arg = args.args[0].print_prec(cfg, alloc, 1);
+            let res = args.args[1].print_prec(cfg, alloc, 0);
+            let fun = arg.append(alloc.space()).append(ARROW).append(alloc.space()).append(res);
+            if prec == 0 {
+                fun
+            } else {
+                fun.parens()
+            }
+        } else {
+            let psubst = if args.is_empty() { alloc.nil() } else { args.print(cfg, alloc) };
+            alloc.typ(&name.id).append(psubst)
+        }
+    }
+}
+
+/// Implement Zonk for TypCtor
+impl Zonk for TypCtor {
+    fn zonk(
+        &mut self,
+        meta_vars: &crate::HashMap<MetaVar, crate::MetaVarState>,
+    ) -> Result<(), ZonkError> {
+        let TypCtor { span: _, name: _, args } = self;
+        args.zonk(meta_vars)
+    }
+}
+
+impl ContainsMetaVars for TypCtor {
+    fn contains_metavars(&self) -> bool {
+        let TypCtor { span: _, name: _, args } = self;
+
+        args.contains_metavars()
+    }
+}
