c306d3d35331b94a2c1438043cb366ab243ae480
Tim Süberkrüb <dev@timsueberkrueb.io>
2024-11-13 15:44:13 +0100

    Fix lookup functions (#368)
    * Fix driver lookup functions
    
    * Fix elaborator lookup functions

--- lang/driver/src/info/lookup.rs
+++ lang/driver/src/info/lookup.rs
@@ -4,101 +4,52 @@ use url::Url;
 use crate::Database;
 
 pub fn lookup_decl<'a>(db: &'a Database, name: &IdBound) -> Option<(Url, &'a Decl)> {
-    for uri in db.ust.keys() {
-        match db.ust.get_unless_stale(uri) {
-            Some(Ok(module)) => {
-                if let Some(decl) = module.decls.iter().find(|decl| decl.ident() == name) {
-                    return Some((uri.clone(), decl));
-                }
-                continue;
-            }
-            _ => continue,
-        }
-    }
-    None
+    let module = db.ust.get_unless_stale(&name.uri)?.as_ref().ok()?;
+    let decl = module.decls.iter().find(|decl| decl.ident() == name)?;
+    Some((name.uri.clone(), decl))
 }
 
 pub fn lookup_ctor<'a>(db: &'a Database, name: &IdBound) -> Option<(Url, &'a Ctor)> {
-    for uri in db.ust.keys() {
-        match db.ust.get_unless_stale(uri) {
-            Some(Ok(module)) => {
-                if let Some(ctor) = module.decls.iter().find_map(|decl| match decl {
-                    Decl::Data(data) => data.ctors.iter().find(|ctor| &ctor.name == name),
-                    _ => None,
-                }) {
-                    return Some((uri.clone(), ctor));
-                }
-            }
-            _ => continue,
-        }
-    }
-    None
+    let module = db.ust.get_unless_stale(&name.uri)?.as_ref().ok()?;
+    let ctor = module.decls.iter().find_map(|decl| match decl {
+        Decl::Data(data) => data.ctors.iter().find(|ctor| &ctor.name == name),
+        _ => None,
+    })?;
+    Some((name.uri.clone(), ctor))
 }
 
 pub fn lookup_codef<'a>(db: &'a Database, name: &IdBound) -> Option<(Url, &'a Codef)> {
-    for uri in db.ust.keys() {
-        match db.ust.get_unless_stale(uri) {
-            Some(Ok(module)) => {
-                if let Some(codef) = module.decls.iter().find_map(|decl| match decl {
-                    Decl::Codef(codef) if codef.name == *name => Some(codef),
-                    _ => None,
-                }) {
-                    return Some((uri.clone(), codef));
-                }
-            }
-            _ => continue,
-        }
-    }
-    None
+    let module = db.ust.get_unless_stale(&name.uri)?.as_ref().ok()?;
+    let codef = module.decls.iter().find_map(|decl| match decl {
+        Decl::Codef(codef) if codef.name == *name => Some(codef),
+        _ => None,
+    })?;
+    Some((name.uri.clone(), codef))
 }
 
 pub fn lookup_let<'a>(db: &'a Database, name: &IdBound) -> Option<(Url, &'a Let)> {
-    for uri in db.ust.keys() {
-        match db.ust.get_unless_stale(uri) {
-            Some(Ok(module)) => {
-                if let Some(tl_let) = module.decls.iter().find_map(|decl| match decl {
-                    Decl::Let(tl_let) if tl_let.name == *name => Some(tl_let),
-                    _ => None,
-                }) {
-                    return Some((uri.clone(), tl_let));
-                }
-            }
-            _ => continue,
-        }
-    }
-    None
+    let module = db.ust.get_unless_stale(&name.uri)?.as_ref().ok()?;
+    let tl_let = module.decls.iter().find_map(|decl| match decl {
+        Decl::Let(tl_let) if tl_let.name == *name => Some(tl_let),
+        _ => None,
+    })?;
+    Some((name.uri.clone(), tl_let))
 }
 
 pub fn lookup_dtor<'a>(db: &'a Database, name: &IdBound) -> Option<(Url, &'a Dtor)> {
-    for uri in db.ust.keys() {
-        match db.ust.get_unless_stale(uri) {
-            Some(Ok(module)) => {
-                if let Some(dtor) = module.decls.iter().find_map(|decl| match decl {
-                    Decl::Codata(codata) => codata.dtors.iter().find(|dtor| &dtor.name == name),
-                    _ => None,
-                }) {
-                    return Some((uri.clone(), dtor));
-                }
-            }
-            _ => continue,
-        }
-    }
-    None
+    let module = db.ust.get_unless_stale(&name.uri)?.as_ref().ok()?;
+    let dtor = module.decls.iter().find_map(|decl| match decl {
+        Decl::Codata(codata) => codata.dtors.iter().find(|dtor| &dtor.name == name),
+        _ => None,
+    })?;
+    Some((name.uri.clone(), dtor))
 }
 
 pub fn lookup_def<'a>(db: &'a Database, name: &IdBound) -> Option<(Url, &'a Def)> {
-    for uri in db.ust.keys() {
-        match db.ust.get_unless_stale(uri) {
-            Some(Ok(module)) => {
-                if let Some(def) = module.decls.iter().find_map(|decl| match decl {
-                    Decl::Def(def) if def.name == *name => Some(def),
-                    _ => None,
-                }) {
-                    return Some((uri.clone(), def));
-                }
-            }
-            _ => continue,
-        }
-    }
-    None
+    let module = db.ust.get_unless_stale(&name.uri)?.as_ref().ok()?;
+    let def = module.decls.iter().find_map(|decl| match decl {
+        Decl::Def(def) if def.name == *name => Some(def),
+        _ => None,
+    })?;
+    Some((name.uri.clone(), def))
 }
--- lang/elaborator/src/typechecker/type_info_table/lookup.rs
+++ lang/elaborator/src/typechecker/type_info_table/lookup.rs
@@ -6,22 +6,25 @@ use super::{
 
 impl TypeInfoTable {
     pub fn lookup_data(&self, name: &IdBound) -> Result<&Data, TypeError> {
-        for map in self.map.values() {
-            if let Some(data) = map.map_data.get(&name.id) {
-                return Ok(data);
-            }
+        let map = self.map.get(&name.uri).ok_or(TypeError::Impossible {
+            message: format!("Module with URI {} not found", name.uri),
+            span: None,
+        })?;
+        if let Some(data) = map.map_data.get(&name.id) {
+            return Ok(data);
         }
         Err(TypeError::Impossible {
             message: format!("Top-level data type {name} not found"),
             span: None,
         })
     }
-
     pub fn lookup_codata(&self, name: &IdBound) -> Result<&Codata, TypeError> {
-        for map in self.map.values() {
-            if let Some(codata) = map.map_codata.get(&name.id) {
-                return Ok(codata);
-            }
+        let map = self.map.get(&name.uri).ok_or(TypeError::Impossible {
+            message: format!("Module with URI {} not found", name.uri),
+            span: None,
+        })?;
+        if let Some(codata) = map.map_codata.get(&name.id) {
+            return Ok(codata);
         }
         Err(TypeError::Impossible {
             message: format!("Top-level codata type {name} not found"),
@@ -30,13 +33,15 @@ impl TypeInfoTable {
     }
 
     pub fn lookup_ctor_or_codef(&self, name: &IdBound) -> Result<CtorMeta, TypeError> {
-        for map in self.map.values() {
-            if let Some(meta) = map.map_ctor.get(&name.id) {
-                return Ok(meta.clone());
-            }
-            if let Some(meta) = map.map_codef.get(&name.id) {
-                return Ok(meta.to_ctor().clone());
-            }
+        let map = self.map.get(&name.uri).ok_or(TypeError::Impossible {
+            message: format!("Module with URI {} not found", name.uri),
+            span: None,
+        })?;
+        if let Some(meta) = map.map_ctor.get(&name.id) {
+            return Ok(meta.clone());
+        }
+        if let Some(meta) = map.map_codef.get(&name.id) {
+            return Ok(meta.to_ctor().clone());
         }
         Err(TypeError::Impossible {
             message: format!("Top-level ctor or codef {name} not found"),
@@ -45,13 +50,15 @@ impl TypeInfoTable {
     }
 
     pub fn lookup_dtor_or_def(&self, name: &IdBound) -> Result<DtorMeta, TypeError> {
-        for map in self.map.values() {
-            if let Some(meta) = map.map_dtor.get(&name.id) {
-                return Ok(meta.clone());
-            }
-            if let Some(meta) = map.map_def.get(&name.id) {
-                return Ok(meta.to_dtor().clone());
-            }
+        let map = self.map.get(&name.uri).ok_or(TypeError::Impossible {
+            message: format!("Module with URI {} not found", name.uri),
+            span: None,
+        })?;
+        if let Some(meta) = map.map_dtor.get(&name.id) {
+            return Ok(meta.clone());
+        }
+        if let Some(meta) = map.map_def.get(&name.id) {
+            return Ok(meta.to_dtor().clone());
         }
         Err(TypeError::Impossible {
             message: format!("Top-level dtor or def {name} not found"),
@@ -60,10 +67,12 @@ impl TypeInfoTable {
     }
 
     pub fn lookup_let(&self, name: &IdBound) -> Result<&LetMeta, TypeError> {
-        for map in self.map.values() {
-            if let Some(meta) = map.map_let.get(&name.id) {
-                return Ok(meta);
-            }
+        let map = self.map.get(&name.uri).ok_or(TypeError::Impossible {
+            message: format!("Module with URI {} not found", name.uri),
+            span: None,
+        })?;
+        if let Some(meta) = map.map_let.get(&name.id) {
+            return Ok(meta);
         }
         Err(TypeError::Impossible {
             message: format!("Top-level let {name} not found"),
@@ -72,10 +81,12 @@ impl TypeInfoTable {
     }
 
     pub fn lookup_tyctor(&self, name: &IdBound) -> Result<&TyCtorMeta, TypeError> {
-        for map in self.map.values() {
-            if let Some(meta) = map.map_tyctor.get(&name.id) {
-                return Ok(meta);
-            }
+        let map = self.map.get(&name.uri).ok_or(TypeError::Impossible {
+            message: format!("Module with URI {} not found", name.uri),
+            span: None,
+        })?;
+        if let Some(meta) = map.map_tyctor.get(&name.id) {
+            return Ok(meta);
         }
         Err(TypeError::Impossible {
             message: format!("Top-level tyctor {name} not found"),
@@ -84,10 +95,12 @@ impl TypeInfoTable {
     }
 
     pub fn lookup_codef(&self, name: &IdBound) -> Result<&CodefMeta, TypeError> {
-        for map in self.map.values() {
-            if let Some(meta) = map.map_codef.get(&name.id) {
-                return Ok(meta);
-            }
+        let map = self.map.get(&name.uri).ok_or(TypeError::Impossible {
+            message: format!("Module with URI {} not found", name.uri),
+            span: None,
+        })?;
+        if let Some(meta) = map.map_codef.get(&name.id) {
+            return Ok(meta);
         }
         Err(TypeError::Impossible {
             message: format!("Top-level codef {name} not found"),
@@ -96,10 +109,12 @@ impl TypeInfoTable {
     }
 
     pub fn lookup_ctor(&self, name: &IdBound) -> Result<&CtorMeta, TypeError> {
-        for map in self.map.values() {
-            if let Some(meta) = map.map_ctor.get(&name.id) {
-                return Ok(meta);
-            }
+        let map = self.map.get(&name.uri).ok_or(TypeError::Impossible {
+            message: format!("Module with URI {} not found", name.uri),
+            span: None,
+        })?;
+        if let Some(meta) = map.map_ctor.get(&name.id) {
+            return Ok(meta);
         }
         Err(TypeError::Impossible {
             message: format!("Top-level ctor {name} not found"),
@@ -108,10 +123,12 @@ impl TypeInfoTable {
     }
 
     pub fn lookup_def(&self, name: &IdBound) -> Result<&DefMeta, TypeError> {
-        for map in self.map.values() {
-            if let Some(meta) = map.map_def.get(&name.id) {
-                return Ok(meta);
-            }
+        let map = self.map.get(&name.uri).ok_or(TypeError::Impossible {
+            message: format!("Module with URI {} not found", name.uri),
+            span: None,
+        })?;
+        if let Some(meta) = map.map_def.get(&name.id) {
+            return Ok(meta);
         }
         Err(TypeError::Impossible {
             message: format!("Top-level def {name} not found"),
@@ -120,10 +137,12 @@ impl TypeInfoTable {
     }
 
     pub fn lookup_dtor(&self, name: &IdBound) -> Result<&DtorMeta, TypeError> {
-        for map in self.map.values() {
-            if let Some(meta) = map.map_dtor.get(&name.id) {
-                return Ok(meta);
-            }
+        let map = self.map.get(&name.uri).ok_or(TypeError::Impossible {
+            message: format!("Module with URI {} not found", name.uri),
+            span: None,
+        })?;
+        if let Some(meta) = map.map_dtor.get(&name.id) {
+            return Ok(meta);
         }
         Err(TypeError::Impossible {
             message: format!("Top-level dtor {name} not found"),
