cca682c13f726c162e7ebb24e826d4acada22326
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2024-11-20 12:49:47 +0100

    Added docs structure

--- app/src/cli/doc.rs
+++ app/src/cli/doc.rs
@@ -1,29 +1,5 @@
-use docs::open;
-use docs::write_html;
-use driver::paths::DOCS_PATH;
-use std::path::PathBuf;
+pub use docs::doc::Args;
+pub use docs::doc::exec;
 
-#[derive(clap::Args)]
-pub struct Args {
-    #[clap(value_parser, value_name = "FILE")]
-    filepath: PathBuf,
-    #[clap(long, num_args = 0)]
-    open: bool,
-}
 
-pub fn exec(cmd: Args) -> miette::Result<()> {
-    let htmlpath = get_path(&cmd);
-    let filepath = &cmd.filepath;
-    write_html(filepath, &htmlpath);
-    if cmd.open {
-        open(&htmlpath);
-    }
-    Ok(())
-}
 
-fn get_path(cmd: &Args) -> PathBuf {
-    let path = format!("{}{}", DOCS_PATH, cmd.filepath.file_name().unwrap().to_string_lossy());
-    let mut fp = PathBuf::from(path);
-    fp.set_extension("html");
-    fp
-}
