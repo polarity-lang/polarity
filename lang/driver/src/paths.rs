793d57304ff92511a0cde84e07944ada8f8d6300
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2024-11-27 20:34:19 +0100

    make CSS_TEMPLATE_PATH static

--- lang/driver/src/paths.rs
+++ lang/driver/src/paths.rs
@@ -4,4 +4,4 @@ pub const DOCS_PATH: &str = "target_pol/docs/";
 
 pub const CSS_PATH: &str = "target_pol/docs/style.css";
 
-pub const CSS_TEMPLATE_PATH: &str = "lang/docs/templates/style.css";
+pub const CSS_TEMPLATE_PATH: &str = include_str!("../../docs/templates/style.css");
