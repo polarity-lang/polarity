22cbe576ecd6d455df8e00fd81c77d79dd1bf05b
Tim Süberkrüb <dev@timsueberkrueb.io>
2024-11-29 22:54:37 +0100

    Improve script output

--- scripts/check_examples_index.sh
+++ scripts/check_examples_index.sh
@@ -9,9 +9,9 @@ index_paths=$(jq -r '.[].path' examples/index.json | sort)
 
 # Compare the two lists
 if diff <(echo "$pol_files") <(echo "$index_paths") >/dev/null; then
-  echo "Success: index.json and .pol files are in sync."
+  echo "Success: examples/index.json and examples/*.pol files are in sync."
 else
-  echo "Error: index.json and .pol files in examples/ are not in sync."
+  echo "Error: examples/index.json and examples/*.pol are not in sync."
   echo "Diff:"
   diff <(echo "$pol_files") <(echo "$index_paths")
 fi
