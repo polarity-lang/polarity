3c7a0a6294b13295294bba7a2235f4402c8bafeb
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2024-12-03 21:41:15 +0100

    Askama for HTML (#399)
    * use askama for Html generation
    
    * make write_html async
    
    * add template parameter to code.html title
    
    * create path to css recursively
    
    * Revert "create path to css recursively"
    
    This reverts commit 645ed5f17f546c1ec8cf66a89f5bce7860234951.
    
    * create path to css recursively
    
    * change unnecessary exclamation mark
    
    * make CSS_TEMPLATE_PATH static
    
    * delete unused selector
    
    * delete decode_html_entities
    
    * change template name
    
    * escape Html in html.rs
    
    * add newline
    
    * change whitespaces
    
    * Remove unnecessary boxing
    
    ---------
    
    Co-authored-by: Tim Süberkrüb <dev@timsueberkrueb.io>

--- lang/docs/src/doc.rs
+++ lang/docs/src/doc.rs
@@ -1,35 +1,13 @@
 use std::fs;
-use std::io;
 use std::io::prelude::*;
-use std::path::Path;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
+use askama::Template;
 use opener;
 
-use driver::paths::CSS_PATH;
+use driver::paths::{CSS_PATH, CSS_TEMPLATE_PATH};
 use driver::Database;
 use printer::{Print, PrintCfg};
-
-const HTML_END: &str = " </code></pre>
-    </div></body></html>";
-
-fn html_start(filepath: &Path) -> String {
-    format!(
-        "<!DOCTYPE html>
-<html lang=\"en\">
-<head>
-    <meta charset=\"UTF-8\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <title>{filename}</title>
-    <link rel=\"stylesheet\" href=\"style.css\">
-</head>
-<body>
-<div>
-        <h1>{filename}</h1>
-        <pre><code>",
-        filename = filepath.file_name().unwrap().to_string_lossy()
-    )
-}
 
 pub async fn write_html(filepath: &PathBuf, htmlpath: &PathBuf) {
     let mut db = Database::from_path(filepath);
@@ -38,17 +16,18 @@ pub async fn write_html(filepath: &PathBuf, htmlpath: &PathBuf) {
     let cfg = PrintCfg::default();
 
     if !Path::new(CSS_PATH).exists() {
-        write_css();
+        fs::create_dir_all(Path::new(CSS_PATH).parent().unwrap())
+            .expect("Failed to create CSS directory");
+        fs::write(CSS_PATH, CSS_TEMPLATE_PATH).expect("Failed to create CSS file");
     }
-    let mut stream = Box::new(fs::File::create(htmlpath).expect("Failed to create file"));
-    stream.write_all(html_start(filepath).as_bytes()).expect("Failed to write to file");
-    print_prg(&prg, &cfg, &mut stream);
-    stream.write_all(HTML_END.as_bytes()).expect("Failed to write to file");
-}
 
-fn print_prg<W: io::Write>(prg: &ast::Module, cfg: &PrintCfg, stream: &mut W) {
-    prg.print_html(cfg, stream).expect("Failed to print to stdout");
-    println!();
+    let mut stream = fs::File::create(htmlpath).expect("Failed to create file");
+
+    let code = prg.print_html_to_string(Some(&cfg));
+    let title = filepath.file_name().unwrap().to_str().unwrap();
+    let output = generate_html(title, &code);
+
+    stream.write_all(output.as_bytes()).expect("Failed to write to file");
 }
 
 pub fn open(filepath: &PathBuf) {
@@ -56,72 +35,14 @@ pub fn open(filepath: &PathBuf) {
     opener::open(&absolute_path).unwrap();
 }
 
-pub fn write_css() {
-    let css_content = "
-    body {
-        font-family: 'Courier New', Courier, monospace;
-        background-color: #000;
-        color: #fff;
-        margin: 0;
-        padding: 20px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-    }
+#[derive(Template)]
+#[template(path = "module.html", escape = "none")]
+struct ModuleTemplate<'a> {
+    title: &'a str,
+    code: &'a str,
+}
 
-    h1 {
-        font-size: 2.5em;
-        text-align: center;
-        color: #fff;
-        margin-bottom: 20px;
-    }
-
-    pre {
-        background-color: #111;
-        color: #f8f8f2;
-        padding: 20px;
-        border-radius: 8px;
-        width: 80%;
-        overflow-x: auto;
-        font-size: 1.1em;
-        line-height: 1.6;
-        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.5);
-    }
-
-    code {
-        font-family: 'Courier New', Courier, monospace;
-    }
-
-    /* Syntax Highlighting Styling */
-    .keyword {
-        color: #ff79c6;
-        font-weight: bold;
-    }
-
-    .type,
-    .title {
-        color: #8be9fd;
-    }
-
-    .dtor {
-        color: #ff5555;
-    }
-
-    .ctor {
-        color: #ff79c6;
-    }
-
-    .string {
-        color: #f1fa8c;
-    }
-
-    .comment {
-        color: #6272a4;
-        font-style: italic;
-    }
-    ";
-    fs::create_dir_all(Path::new(CSS_PATH).parent().unwrap())
-        .expect("Failed to create directories");
-    fs::write(CSS_PATH, css_content).expect("Failed to write CSS file");
+fn generate_html(title: &str, code: &str) -> String {
+    let template = ModuleTemplate { title, code };
+    template.render().unwrap()
 }
--- lang/driver/src/paths.rs
+++ lang/driver/src/paths.rs
@@ -3,3 +3,5 @@ pub const TARGET_PATH: &str = "target_pol/";
 pub const DOCS_PATH: &str = "target_pol/docs/";
 
 pub const CSS_PATH: &str = "target_pol/docs/style.css";
+
+pub const CSS_TEMPLATE_PATH: &str = include_str!("../../docs/templates/style.css");
