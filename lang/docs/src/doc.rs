b69dffe34c092ff032b0a0e7f7c14ab5b5c3f50e
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2025-02-08 17:20:03 +0100

    Add std library modules to generated docs (#468)
    * add std files to document generation
    
    * add index.jason
    
    * generate docs from json
    
    * add target to json and change doc to generate correct
    
    * delete debug print in writes module
    
    * delete json
    
    * add foldersystem for target_pol
    
    * change list path
    
    * repair cargo.toml and fmt
    
    * expect for no parent folder case
    
    * delete unused variable
    
    * rename gernerate_module to generate module_docs
    
    * change output_file to html_file
    
    * simplify with fs::write
    
    * add subfolder to html generate docs
    
    * Move slash around
    
    * clippy changes
    
    ---------
    
    Co-authored-by: Tim Süberkrüb <dev@timsueberkrueb.io>

--- lang/docs/src/doc.rs
+++ lang/docs/src/doc.rs
@@ -1,11 +1,10 @@
 use std::fs;
-use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 
 use askama::Template;
 use opener;
 
-use driver::paths::{CSS_PATH, CSS_TEMPLATE_PATH, DOCS_PATH, EXAMPLE_PATH};
+use driver::paths::{CSS_PATH, CSS_TEMPLATE_PATH, DOCS_PATH};
 use driver::Database;
 
 use crate::generate_docs::GenerateDocs;
@@ -19,11 +18,48 @@ pub async fn write_html() {
     write_modules().await;
 }
 
+async fn write_modules() {
+    let folders: Vec<&Path> = vec![Path::new("examples/"), Path::new("std")];
+    let path_list = get_all_filepaths(folders);
+    let list = list_to_html(&path_list);
+    for path in path_list {
+        let mut db = Database::from_path(&path);
+        let uri = db.resolve_path(&path).expect("Failed to resolve path");
+        let prg = db.ust(&uri).await.expect("Failed to get UST");
+
+        let code = prg.generate_docs();
+        let content = generate_module_docs(path.file_stem().unwrap().to_str().unwrap(), &code);
+        let html_file = generate_html(path.file_stem().unwrap().to_str().unwrap(), &list, &content);
+
+        let target_path = get_target_path(&path);
+        if let Some(parent) = target_path.parent() {
+            fs::create_dir_all(parent).expect("Failed to create directories");
+        }
+
+        fs::write(target_path, html_file.as_bytes()).expect("Failed to write to file");
+    }
+}
+
 pub fn open(filepath: &PathBuf) {
     let absolute_path = fs::canonicalize(filepath).expect("Failed to get absolute path");
     opener::open(&absolute_path).unwrap();
 }
 
+fn generate_module_docs(title: &str, content: &str) -> String {
+    let template = ModuleTemplate { title, content };
+    template.render().unwrap()
+}
+#[derive(Template)]
+#[template(path = "module.html", escape = "none")]
+struct ModuleTemplate<'a> {
+    title: &'a str,
+    content: &'a str,
+}
+
+fn generate_html(title: &str, list: &str, code: &str) -> String {
+    let template = IndexTemplate { title, list, code, start: title };
+    template.render().unwrap()
+}
 #[derive(Template)]
 #[template(path = "index.html", escape = "none")]
 struct IndexTemplate<'a> {
@@ -33,75 +69,62 @@ struct IndexTemplate<'a> {
     start: &'a str,
 }
 
-fn generate_html(title: &str, list: &str, code: &str) -> String {
-    let template = IndexTemplate { title, list, code, start: title };
-    template.render().unwrap()
-}
-
-#[derive(Template)]
-#[template(path = "module.html", escape = "none")]
-struct ModuleTemplate<'a> {
-    title: &'a str,
-    content: &'a str,
-}
-
-fn generate_module(title: &str, content: &str) -> String {
-    let template = ModuleTemplate { title, content };
-    template.render().unwrap()
-}
-
-async fn write_modules() {
-    let example_path = Path::new(EXAMPLE_PATH);
-    let mut all_modules = String::new();
-    let list = file_list(get_files(Path::new(EXAMPLE_PATH)));
-    for file in get_files(example_path) {
-        let mut db = Database::from_path(&file);
-        let uri = db.resolve_path(&file).expect("Failed to resolve path");
-        let prg = db.ust(&uri).await.expect("Failed to get UST");
-
-        let title = file.file_stem().unwrap().to_str().unwrap();
-        let title = title.replace('_', " ");
-        let code = prg.generate_docs();
-        let content = generate_module(&title, &code);
-        let output_file = generate_html(&title, &list, &content);
-
-        let htmlpath = get_path(&file);
-        let mut stream = fs::File::create(htmlpath).expect("Failed to create file");
-
-        stream.write_all(output_file.as_bytes()).expect("Failed to write to file");
-
-        all_modules.push_str(&output_file);
-    }
-}
-
-fn get_files(path: &Path) -> Vec<PathBuf> {
-    let mut files = Vec::new();
-    if path.is_dir() {
-        for entry in fs::read_dir(path).expect("Failed to read directory") {
-            let entry = entry.expect("Failed to read entry");
+fn get_filepaths_from_folder(folder: &Path) -> Vec<PathBuf> {
+    let mut filepaths = Vec::new();
+    if folder.is_dir() {
+        for entry in fs::read_dir(folder).expect("Failed to read directory") {
+            let entry = entry.expect("Failed to get directory entry");
             let path = entry.path();
-            if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("pol") {
-                files.push(path);
+            if path.is_file() && path.extension().and_then(|ext| ext.to_str()) == Some("pol") {
+                filepaths.push(path);
+            } else if path.is_dir() {
+                filepaths.append(&mut get_filepaths_from_folder(&path));
             }
         }
     }
-    files
+    filepaths
 }
 
-fn file_list(files: Vec<PathBuf>) -> String {
-    let mut list = String::new();
-    for file in files {
-        let name = file.file_stem().unwrap().to_str().unwrap();
-        let path = name.to_string() + ".html";
-        let name = name.replace('_', " ");
-        list.push_str(&format!("<li><a href={}>{}</a></li>", &path, name));
+fn get_all_filepaths(folders: Vec<&Path>) -> Vec<PathBuf> {
+    let mut all_filepaths = Vec::new();
+    for folder in folders {
+        let mut filepaths = get_filepaths_from_folder(folder);
+        all_filepaths.append(&mut filepaths);
     }
-    list
+    all_filepaths
 }
 
-fn get_path(filepath: &Path) -> PathBuf {
-    let mut path =
-        Path::new(DOCS_PATH).join(filepath.file_name().unwrap().to_string_lossy().as_ref());
-    path.set_extension("html");
-    path
+pub fn get_target_path(path: &Path) -> PathBuf {
+    let mut new_path = PathBuf::from(DOCS_PATH);
+    new_path.push(get_parent_folder(path));
+    if let Some(stem) = path.file_stem() {
+        new_path.push(stem);
+        new_path.set_extension("html");
+    }
+    if let Some(parent) = new_path.parent() {
+        fs::create_dir_all(parent).expect("Failed to create directories");
+    }
+    new_path
+}
+
+pub fn list_to_html(list: &Vec<PathBuf>) -> String {
+    let mut html = String::new();
+    for path in list {
+        html.push_str(&path_to_html(&get_target_path(path)));
+    }
+    html
+}
+
+fn path_to_html(path: &Path) -> String {
+    format!(
+        "<li><a href=../{}/{}>{}</a></li>",
+        get_parent_folder(path),
+        path.file_name().unwrap().to_string_lossy(),
+        path.file_stem().unwrap().to_string_lossy()
+    )
+}
+
+fn get_parent_folder(path: &Path) -> String {
+    let parent = path.parent().expect("Failed to get parent directory");
+    parent.file_stem().expect("Failed to get folder name").to_string_lossy().to_string()
 }
--- lang/docs/src/lib.rs
+++ lang/docs/src/lib.rs
@@ -2,5 +2,6 @@ pub mod doc;
 pub mod generate;
 pub mod generate_docs;
 
+pub use doc::get_target_path;
 pub use doc::open;
 pub use doc::write_html;
