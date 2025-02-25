4ed1b2101f88b67653a4e00021886c10de70eef6
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2025-02-25 13:38:07 +0100

    minor form changes

--- lang/docs/src/lib.rs
+++ lang/docs/src/lib.rs
@@ -2,13 +2,13 @@ pub mod doc;
 pub mod generate;
 pub mod generate_docs;
 pub mod printer;
-pub mod util;
 pub mod sidebar;
+pub mod util;
 
 mod render;
 pub use doc::write_html;
 pub use printer::*;
+pub use sidebar::generate_html_from_paths;
 pub use util::get_target_path;
 pub use util::open;
 pub use util::trim_windows_path_prefix;
-pub use sidebar::generate_html_from_paths;
--- lang/docs/src/sidebar.rs
+++ lang/docs/src/sidebar.rs
@@ -1,4 +1,5 @@
-use std::{fs::{self, canonicalize}, path::{Path, PathBuf}};
+use std::fs;
+use std::path::{Path, PathBuf};
 
 use crate::util::{get_target_path, trim_windows_path_prefix};
 
@@ -24,12 +25,7 @@ pub struct FolderStructure {
 
 impl FolderStructure {
     fn new(name: String, path: PathBuf) -> Self {
-        Self {
-            name,
-            files: Vec::new(),
-            subfolders: Vec::new(),
-            path,
-        }
+        Self { name, files: Vec::new(), subfolders: Vec::new(), path }
     }
 
     fn add_file(&mut self, file: FileStructure) {
@@ -47,10 +43,14 @@ impl FolderStructure {
         if !self.files.is_empty() || !self.subfolders.is_empty() {
             html.push_str("<ul>");
             for file in &self.files {
-                let canonical_path = fs::canonicalize(&file.path).expect("Failed to canonicalize path");
+                let canonical_path =
+                    fs::canonicalize(&file.path).expect("Failed to canonicalize path");
                 let target_path = get_target_path(&canonical_path);
                 let trimed_path = trim_windows_path_prefix(&target_path);
-                html.push_str(&format!("<li class=\"file\"><a href=\"{}\">{}</a></li>", trimed_path, file.name));
+                html.push_str(&format!(
+                    "<li class=\"file\"><a href=\"{}\">{}</a></li>",
+                    trimed_path, file.name
+                ));
             }
             for subfolder in &self.subfolders {
                 html.push_str(&subfolder.generate_html());
@@ -73,8 +73,7 @@ fn build_folder_structure(path: &Path) -> FolderStructure {
             let entry = entry.expect("Failed to get directory entry");
             let path = entry.path();
             if path.is_file() && path.extension().and_then(|ext| ext.to_str()) == Some("pol") {
-                folder_structure.add_file(
-                    FileStructure::new(
+                folder_structure.add_file(FileStructure::new(
                     path.file_stem().unwrap().to_string_lossy().to_string(),
                     path.to_path_buf(),
                 ));
