4ade8b19188008d8a30fe337b1288192871b56ac
Tim Süberkrüb <dev@timsueberkrueb.io>
2025-03-31 21:09:59 +0200

    Validate binding structure of infix decls

--- lang/lowering/src/lower/decls/infix_declaration.rs
+++ lang/lowering/src/lower/decls/infix_declaration.rs
@@ -1,3 +1,4 @@
+use ast::ctx::BindContext;
 use parser::cst;
 
 use super::super::*;
@@ -7,6 +8,11 @@ impl Lower for cst::decls::Infix {
 
     fn lower(&self, ctx: &mut Ctx) -> Result<Self::Target, LoweringError> {
         let cst::decls::Infix { span, doc, lhs, rhs } = self;
+
+        let left_binder = lhs.lhs.lower(ctx)?;
+        let right_binder = lhs.rhs.lower(ctx)?;
+
+        ctx.bind_iter([left_binder, right_binder].into_iter(), |ctx| rhs.lower(ctx))?;
 
         Ok(ast::Infix {
             span: Some(*span),
