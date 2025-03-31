2e6559b1bdaa5df21d0dd9be4bf252aa2ac352a1
David Binder <david.binder@uni-tuebingen.de>
2025-03-31 14:24:22 +0100

    Make testsuite run through again

--- lang/lowering/src/lower/decls/infix_declaration.rs
+++ lang/lowering/src/lower/decls/infix_declaration.rs
@@ -3,9 +3,17 @@ use parser::cst;
 use super::super::*;
 
 impl Lower for cst::decls::Infix {
-    type Target = ast::Decl;
+    type Target = ast::Infix;
 
-    fn lower(&self, _ctx: &mut Ctx) -> Result<Self::Target, LoweringError> {
-        todo!()
+    fn lower(&self, ctx: &mut Ctx) -> Result<Self::Target, LoweringError> {
+        let cst::decls::Infix { span, doc, lhs, rhs } = self;
+
+        Ok(ast::Infix {
+            span: Some(*span),
+            doc: doc.lower(ctx)?,
+            attr: Default::default(),
+            lhs: lhs.operator.id.clone(),
+            rhs: rhs.name.id.clone(),
+        })
     }
 }
--- lang/lowering/src/lower/decls/mod.rs
+++ lang/lowering/src/lower/decls/mod.rs
@@ -73,7 +73,7 @@ impl Lower for cst::decls::Decl {
             cst::decls::Decl::Def(def) => def.lower(ctx)?.into(),
             cst::decls::Decl::Codef(codef) => codef.lower(ctx)?.into(),
             cst::decls::Decl::Let(tl_let) => tl_let.lower(ctx)?.into(),
-            cst::decls::Decl::Infix(infix) => infix.lower(ctx)?,
+            cst::decls::Decl::Infix(infix) => infix.lower(ctx)?.into(),
         };
         Ok(decl)
     }
