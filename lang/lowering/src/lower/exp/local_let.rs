ee5b31d1c5262696db58a0d0b4dcf0b5b450de9c
David Binder <david.binder@uni-tuebingen.de>
2025-05-05 11:57:59 +0200

    Add local lets to parser

--- lang/lowering/src/lower/exp/local_let.rs
+++ lang/lowering/src/lower/exp/local_let.rs
@@ -1,11 +1,17 @@
+use miette_util::ToMiette;
 use parser::cst;
 
-use crate::lower::Lower;
+use crate::{lower::Lower, LoweringError};
 
 impl Lower for cst::exp::LocalLet {
     type Target = ast::Exp;
 
     fn lower(&self, _ctx: &mut crate::Ctx) -> crate::LoweringResult<Self::Target> {
-        todo!()
+        let cst::exp::LocalLet { span, .. } = self;
+        Err(LoweringError::Impossible {
+            message: "Lowering of local let expressions not implemented yet".to_string(),
+            span: Some(span.to_miette()),
+        }
+        .into())
     }
 }
