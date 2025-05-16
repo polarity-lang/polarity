78f455624d4ef58f08bc51382e0ba07d68456e04
David Binder <david.binder@uni-tuebingen.de>
2025-05-16 13:31:31 +0100

    Some more code

--- lang/lsp/src/semantic_tokens.rs
+++ lang/lsp/src/semantic_tokens.rs
@@ -1,9 +1,24 @@
+use crate::conversion::FromLsp;
+
 use super::server::*;
 use tower_lsp_server::{jsonrpc, lsp_types::*};
 
 pub async fn semantic_tokens_full(
-    _server: &Server,
-    _params: SemanticTokensParams,
+    server: &Server,
+    params: SemanticTokensParams,
 ) -> jsonrpc::Result<Option<SemanticTokensResult>> {
-    todo!()
+    let text_document = params.text_document;
+    server
+        .client
+        .log_message(
+            MessageType::INFO,
+            format!("SemanticToken request: {}", text_document.uri.from_lsp()),
+        )
+        .await;
+
+    let mut _db = server.database.write().await;
+
+    let tokens: SemanticTokens = SemanticTokens { result_id: None, data: vec![] };
+    let res: SemanticTokensResult = SemanticTokensResult::Tokens(tokens);
+    Ok(Some(res))
 }
