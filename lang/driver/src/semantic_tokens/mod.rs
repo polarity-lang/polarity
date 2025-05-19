f774dec703ab5048333ace6ac93524e6baf0e830
David Binder <david.binder@uni-tuebingen.de>
2025-05-19 17:02:32 +0100

    Work on sem tokens

--- lang/driver/src/semantic_tokens/mod.rs
+++ lang/driver/src/semantic_tokens/mod.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
+pub mod collect;
 pub mod types;
 
+use crate::semantic_tokens::collect::SemTokens;
 use lsp_types::{SemanticToken, SemanticTokenType, SemanticTokensLegend};
 use polarity_lang_ast::{Codata, Codef, Data, Decl, Def, Extern, Let, Module, Note};
-use types::{convert_sem_tokens, SemToken};
+use types::{SemToken, convert_sem_tokens};
 
 /// Describes which semantic tokens and modifiers are emitted by the server.
 /// This legend is used because the actual semantic tokens only contain an index into
@@ -20,76 +22,10 @@ const SEMANTIC_TOKEN_DATATYPE: u32 = 0;
 const SEMANTIC_TOKEN_MODIFIER_NONE: u32 = 0;
 
 pub fn compute_semantic_tokens(module: Arc<Module>) -> Vec<SemanticToken> {
+    // We first collect the tokens with absolute positions.
     let mut tokens: Vec<SemToken> = Vec::new();
-    module.semantic_tokens(&mut tokens);
+    module.collect_tokens(&mut tokens);
+
+    // Then we convert them to relative positions.
     convert_sem_tokens(tokens)
-}
-
-trait SemTokens {
-    fn semantic_tokens(&self, tokens: &mut Vec<SemToken>);
-}
-
-impl<T: SemTokens> SemTokens for Vec<T> {
-    fn semantic_tokens(&self, tokens: &mut Vec<SemToken>) {
-        for x in self {
-            x.semantic_tokens(tokens);
-        }
-    }
-}
-
-impl SemTokens for Module {
-    fn semantic_tokens(&self, tokens: &mut Vec<SemToken>) {
-        let Module { decls, .. } = self;
-        decls.semantic_tokens(tokens);
-    }
-}
-
-impl SemTokens for Decl {
-    fn semantic_tokens(&self, tokens: &mut Vec<SemToken>) {
-        match self {
-            Decl::Extern(ext) => ext.semantic_tokens(tokens),
-            Decl::Note(note) => note.semantic_tokens(tokens),
-            Decl::Data(data) => data.semantic_tokens(tokens),
-            Decl::Codata(codata) => codata.semantic_tokens(tokens),
-            Decl::Def(def) => def.semantic_tokens(tokens),
-            Decl::Codef(codef) => codef.semantic_tokens(tokens),
-            Decl::Let(tl_let) => tl_let.semantic_tokens(tokens),
-            Decl::Infix(_infix) => {}
-        }
-    }
-}
-
-impl SemTokens for Extern {
-    fn semantic_tokens(&self, _tokens: &mut Vec<SemToken>) {}
-}
-
-impl SemTokens for Note {
-    fn semantic_tokens(&self, _tokens: &mut Vec<SemToken>) {}
-}
-
-impl SemTokens for Data {
-    fn semantic_tokens(&self, tokens: &mut Vec<SemToken>) {
-        let Data { name, .. } = self;
-        let st: SemToken = SemToken {
-            span: name.span.unwrap(),
-            typ: types::TokenType::DataType
-        };
-        tokens.push(st);
-    }
-}
-
-impl SemTokens for Codata {
-    fn semantic_tokens(&self, _tokens: &mut Vec<SemToken>) {}
-}
-
-impl SemTokens for Def {
-    fn semantic_tokens(&self, _tokens: &mut Vec<SemToken>) {}
-}
-
-impl SemTokens for Codef {
-    fn semantic_tokens(&self, _tokens: &mut Vec<SemToken>) {}
-}
-
-impl SemTokens for Let {
-    fn semantic_tokens(&self, _tokens: &mut Vec<SemToken>) {}
 }
--- lang/driver/src/semantic_tokens/types.rs
+++ lang/driver/src/semantic_tokens/types.rs
@@ -10,20 +10,19 @@ pub fn token_legend() -> SemanticTokensLegend {
     SemanticTokensLegend { token_types: vec![SemanticTokenType::TYPE], token_modifiers: vec![] }
 }
 
-/// The index of this token type in the legend.
-const SEMANTIC_TOKEN_DATATYPE: u32 = 0;
-
 /// Semantic token modifiers are set using a bitmap.
 /// If no token modifiers are used we therefore use the empty bitmap.
 const SEMANTIC_TOKEN_MODIFIER_NONE: u32 = 0;
 
-
+/// The different semantic token types that we are using.
+/// They have to be converted to index positions in the legend before we transmit them.
 pub enum TokenType {
     DataType,
     CodataType,
 }
 
 impl TokenType {
+    /// The index of this token type in the legend.
     pub fn index(self) -> u32 {
         match self {
             TokenType::DataType => 0,
@@ -32,11 +31,17 @@ impl TokenType {
     }
 }
 
+/// The internal representation of semantic tokens that we use in the server.
+/// This representation uses absolute positions.
 pub struct SemToken {
     pub span: Span,
     pub typ: TokenType,
 }
 
+/// Takes a vector of `SemToken` which contain `Span`'s, i.e. absolute positions,
+/// and converts them to a vector of `SemanticToken` which use a relative encoding.
+/// That is, the position of each `SemanticToken` is relative to the token preceding
+/// it in the vector.
 pub fn convert_sem_tokens(_toks: Vec<SemToken>) -> Vec<SemanticToken> {
     todo!()
 }
