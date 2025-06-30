0aca12991a9be79881a88a0c06b51216f0f47aee
Tim Süberkrüb <dev@timsueberkrueb.io>
2025-06-30 12:26:41 +0200

    Lower local let bindings (#538)

--- lang/lowering/src/lower/exp/local_let.rs
+++ lang/lowering/src/lower/exp/local_let.rs
@@ -1,17 +1,27 @@
-use miette_util::ToMiette;
-use parser::cst;
+use ast::{VarBind, ctx::BindContext};
+use parser::cst::{self, exp::BindingSite};
 
-use crate::{LoweringError, lower::Lower};
+use crate::lower::Lower;
 
 impl Lower for cst::exp::LocalLet {
     type Target = ast::Exp;
 
-    fn lower(&self, _ctx: &mut crate::Ctx) -> crate::LoweringResult<Self::Target> {
-        let cst::exp::LocalLet { span, .. } = self;
-        Err(LoweringError::Impossible {
-            message: "Lowering of local let expressions not implemented yet".to_string(),
-            span: Some(span.to_miette()),
-        }
-        .into())
+    fn lower(&self, ctx: &mut crate::Ctx) -> crate::LoweringResult<Self::Target> {
+        let cst::exp::LocalLet { span, bs, typ, bound, body } = self;
+
+        let name = match bs {
+            BindingSite::Var { span, name } => {
+                VarBind::Var { span: Some(*span), id: name.id.clone() }
+            }
+            BindingSite::Wildcard { span } => VarBind::Wildcard { span: Some(*span) },
+        };
+
+        let typ = typ.lower(ctx)?;
+        let bound = bound.lower(ctx)?;
+
+        ctx.bind_single(name.clone(), |ctx| {
+            let body = body.lower(ctx)?;
+            Ok(ast::LocalLet { span: *span, name, typ, bound, body, inferred_type: None }.into())
+        })
     }
 }
