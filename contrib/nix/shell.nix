dee13f076a77433d8d1c14bf6e12c1dce59a7616
liesnikov <b.liesnikov@gmail.com>
2025-07-02 11:44:06 +0200

    add an env variable to shell.nix for VS Code and rust-analyzer (#545)

--- contrib/nix/shell.nix
+++ contrib/nix/shell.nix
@@ -21,4 +21,8 @@ pkgs.mkShell.override { inherit stdenv; } {
       pkgs.libiconv
       pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
     ];
+
+  # Certain Rust tools won't work without this, for example VS Code with rust analyzer
+  # See https://nixos.wiki/wiki/Rust#Shell.nix_example and https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
