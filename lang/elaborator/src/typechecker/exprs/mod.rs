08d3ec97087f8dc66387ce5b7c295a67e01c87ed
liesnikov <b.liesnikov@gmail.com>
2025-07-05 19:29:20 +0200

    Add typecheck and infer for local lets (#544)
    * add check and infer for lets
    
    * Remove test case
    
    * Format code
    
    ---------
    
    Co-authored-by: Tim Süberkrüb <dev@timsueberkrueb.io>

--- lang/elaborator/src/typechecker/exprs/mod.rs
+++ lang/elaborator/src/typechecker/exprs/mod.rs
@@ -3,6 +3,7 @@ pub mod call;
 pub mod dot_call;
 pub mod hole;
 pub mod local_comatch;
+pub mod local_let;
 pub mod local_match;
 pub mod typ_ctor;
 pub mod type_univ;
@@ -88,10 +89,7 @@ impl CheckInfer for Exp {
             Exp::Hole(e) => Ok(e.check(ctx, t)?.into()),
             Exp::LocalMatch(e) => Ok(e.check(ctx, t)?.into()),
             Exp::LocalComatch(e) => Ok(e.check(ctx, t)?.into()),
-            Exp::LocalLet(local_let) => Err(Box::new(TypeError::Impossible {
-                message: "Checking local let expressions is not implemented yet".to_owned(),
-                span: local_let.span().to_miette(),
-            })),
+            Exp::LocalLet(e) => Ok(e.check(ctx, t)?.into()),
         }
     }
 
@@ -106,10 +104,7 @@ impl CheckInfer for Exp {
             Exp::Hole(e) => Ok(e.infer(ctx)?.into()),
             Exp::LocalMatch(e) => Ok(e.infer(ctx)?.into()),
             Exp::LocalComatch(e) => Ok(e.infer(ctx)?.into()),
-            Exp::LocalLet(local_let) => Err(Box::new(TypeError::Impossible {
-                message: "Inferring local let expressions is not implemented yet".to_owned(),
-                span: local_let.span().to_miette(),
-            })),
+            Exp::LocalLet(e) => Ok(e.infer(ctx)?.into()),
         };
         trace!(
             "{} |- {} => {}",
