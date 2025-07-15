b230830ba68f629c8f257ca057cb7b8ce7306cf5
David Binder <david.binder@uni-tuebingen.de>
2025-07-15 12:46:10 +0100

    Remove closure from normalizer values

--- lang/elaborator/src/normalizer/eval.rs
+++ lang/elaborator/src/normalizer/eval.rs
@@ -442,12 +442,11 @@ impl Eval for LocalComatch {
     type Val = Box<Val>;
 
     fn eval(&self, info_table: &Rc<TypeInfoTable>, env: &mut Env) -> TcResult<Self::Val> {
-        let LocalComatch { span, name, closure, is_lambda_sugar, cases, .. } = self;
+        let LocalComatch { span, name, closure: _, is_lambda_sugar, cases, .. } = self;
         Ok(Box::new(
             val::LocalComatch {
                 span: *span,
                 name: name.clone(),
-                closure: closure.clone(),
                 is_lambda_sugar: *is_lambda_sugar,
                 cases: cases.eval(info_table, env)?,
             }
