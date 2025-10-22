61347cdb400475f64896cdf1b9302b4876d63b58
Rasmus Buurman <rasmus@rbuurman.de>
2025-10-22 12:44:14 +0200

    Add a few comments

--- lang/parser/src/lib.rs
+++ lang/parser/src/lib.rs
@@ -13,10 +13,15 @@ pub fn parse_exp(s: &str) -> Result<Box<cst::exp::Exp>, Vec<ParseError>> {
     let lexer = Lexer::new(s);
     let parser = ExpParser::new();
     let mut errors = Vec::new();
+
+    // `parser.parse` only *returns* an error if error recovery failed (e.g. a lexing error).
+    // Recovered errors are passed into the `errors` Vec.
+    // For the moment, we have to catch all errors at parsing stage here.
     let result = parser.parse(&mut errors, lexer).map_err(|e| vec![e.into()]);
     if !errors.is_empty() {
         return Err(errors.into_iter().map(|e| e.error.into()).collect());
     }
+
     result
 }
 
@@ -24,9 +29,14 @@ pub fn parse_module(uri: Url, s: &str) -> Result<cst::decls::Module, Vec<ParseEr
     let lexer = Lexer::new(s);
     let parser = ModuleContentsParser::new();
     let mut errors = Vec::new();
+
+    // `parser.parse` only *returns* an error if error recovery failed (e.g. a lexing error).
+    // Recovered errors are passed into the `errors` Vec.
+    // For the moment, we have to catch all errors at parsing stage here.
     let (use_decls, decls) = parser.parse(&mut errors, lexer).map_err(|e| vec![e.into()])?;
     if !errors.is_empty() {
         return Err(errors.into_iter().map(|e| e.error.into()).collect());
     }
+
     Ok(cst::decls::Module { uri, use_decls, decls })
 }
