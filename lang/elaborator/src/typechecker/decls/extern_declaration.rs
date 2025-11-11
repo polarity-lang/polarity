d8887a9dd75242c4452a745c06ac887f01d6109e
Rasmus Buurman <rasmus@rbuurman.de>
2025-11-11 16:28:07 +0100

    Check well-formedness of extern declarations

---
+++ lang/elaborator/src/typechecker/decls/extern_declaration.rs
@@ -0,0 +1,36 @@
+//! Checking the well-formedness of extern declarations
+
+use log::trace;
+
+use polarity_lang_ast::*;
+
+use super::CheckToplevel;
+use crate::result::TcResult;
+use crate::typechecker::erasure;
+use crate::typechecker::{
+    ctx::Ctx,
+    exprs::{CheckInfer, InferTelescope},
+};
+
+impl CheckToplevel for Extern {
+    fn check_wf(&self, ctx: &mut Ctx) -> TcResult<Self> {
+        trace!("Checking well-formedness of extern declaration: {}", self.name);
+
+        let Extern { span, doc, name, attr, params, typ } = self;
+
+        params.infer_telescope(ctx, |ctx, mut params_out| {
+            let typ_out = typ.infer(ctx)?;
+
+            erasure::mark_erased_params(&mut params_out);
+
+            Ok(Extern {
+                span: *span,
+                doc: doc.clone(),
+                name: name.clone(),
+                attr: attr.clone(),
+                params: params_out,
+                typ: typ_out,
+            })
+        })
+    }
+}
