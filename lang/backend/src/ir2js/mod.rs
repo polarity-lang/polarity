3a4ff763f5e4d40955a7e3b8ce2a01b2cc60b404
Rasmus Buurman <rasmus@rbuurman.de>
2025-12-16 13:37:49 +0100

    Split ir_to_js into two separate functions

--- lang/backend/src/ir2js/mod.rs
+++ lang/backend/src/ir2js/mod.rs
@@ -4,6 +4,7 @@ use std::io;
 use std::rc::Rc;
 
 use swc_common::SourceMap;
+use swc_ecma_ast as js;
 use swc_ecma_codegen::text_writer::JsWriter;
 use swc_ecma_codegen::{Config as CodegenConfig, Emitter};
 
@@ -19,7 +20,11 @@ use super::ir;
 /// Convert an IR module to JavaScript
 pub fn ir_to_js<W: io::Write>(ir_module: &ir::Module, writer: W) -> BackendResult {
     let js_module = ir_module.to_js_module()?;
+    emit_js(&js_module, writer)
+}
 
+/// Emit a JavaScript module
+fn emit_js<W: io::Write>(js_module: &js::Module, writer: W) -> BackendResult {
     let config = CodegenConfig::default();
     let cm = Rc::new(SourceMap::default());
 
@@ -27,6 +32,6 @@ pub fn ir_to_js<W: io::Write>(ir_module: &ir::Module, writer: W) -> BackendResul
     let mut emitter = Emitter { cfg: config, cm, comments: None, wr: Box::new(js_writer) };
 
     emitter
-        .emit_module(&js_module)
+        .emit_module(js_module)
         .map_err(|e| BackendError::CodegenError(format!("Failed to emit module: {}", e)))
 }
