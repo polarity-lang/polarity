0679f74cd9a86a2856a2a9a4093ce781f785ea52
Rasmus Buurman <rasmus@rbuurman.de>
2025-12-16 13:49:04 +0100

    Separate JS path in target_pol

--- app/src/cli/compile.rs
+++ app/src/cli/compile.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 
-use polarity_lang_driver::{Database, IR_PATH};
+use polarity_lang_driver::{Database, IR_PATH, JS_PATH};
 use polarity_lang_printer::{Print, PrintCfg};
 
 #[derive(clap::Args)]
@@ -23,11 +23,7 @@ async fn generate_ir(db: &mut Database, filepath: &Path) -> Result<(), Vec<miett
     let uri = db.resolve_path(filepath).map_err(|e| vec![e.into()])?;
     let ir = db.ir(&uri).await.map_err(|errs| db.pretty_errors(&uri, errs))?;
 
-    if !Path::new(IR_PATH).exists() {
-        fs::create_dir_all(IR_PATH).expect("Failed to create IR directory");
-    }
-
-    let ir_path = target_path(filepath, "ir");
+    let ir_path = target_path(IR_PATH, filepath, "ir");
     let mut file = fs::File::create(&ir_path).expect("Failed to create file");
     let cfg = PrintCfg::default();
     ir.print_io(&cfg, &mut file).expect("Failed to print to file");
@@ -38,11 +34,7 @@ async fn generate_ir(db: &mut Database, filepath: &Path) -> Result<(), Vec<miett
 async fn generate_js(db: &mut Database, filepath: &Path) -> Result<(), Vec<miette::Report>> {
     let uri = db.resolve_path(filepath).map_err(|e| vec![e.into()])?;
 
-    if !Path::new(IR_PATH).exists() {
-        fs::create_dir_all(IR_PATH).expect("Failed to create IR directory");
-    }
-
-    let js_path = target_path(filepath, "js");
+    let js_path = target_path(JS_PATH, filepath, "js");
     let mut file = fs::File::create(&js_path).expect("Failed to create file");
 
     db.js(&uri, &mut file).await.map_err(|errs| db.pretty_errors(&uri, errs))?;
@@ -50,9 +42,14 @@ async fn generate_js(db: &mut Database, filepath: &Path) -> Result<(), Vec<miett
     Ok(())
 }
 
-fn target_path(filepath: &Path, ext: &str) -> PathBuf {
-    let mut path =
-        Path::new(IR_PATH).join(filepath.file_name().unwrap().to_string_lossy().as_ref());
+fn target_path(base: &str, filepath: &Path, ext: &str) -> PathBuf {
+    let base = Path::new(base);
+
+    if !base.exists() {
+        fs::create_dir_all(base).unwrap_or_else(|e| panic!("Failed to create directory: {e}"));
+    }
+
+    let mut path = Path::new(base).join(filepath.file_name().unwrap().to_string_lossy().as_ref());
     path.set_extension(ext);
     path
 }
--- lang/driver/src/paths.rs
+++ lang/driver/src/paths.rs
@@ -2,6 +2,8 @@ pub const TARGET_PATH: &str = "target_pol/";
 
 pub const IR_PATH: &str = "target_pol/ir/";
 
+pub const JS_PATH: &str = "target_pol/js/";
+
 pub const DOCS_PATH: &str = "target_pol/docs/";
 
 pub const CSS_PATH: &str = "target_pol/docs/style.css";
