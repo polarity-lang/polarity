795d53a45488ca354a9270ecaa379cff3b14809f
Rasmus Buurman <rasmus@rbuurman.de>
2026-03-10 17:13:42 +0100

    Fix scoping in lowering of do blocks

--- lang/lowering/src/lower/exp/do_block.rs
+++ lang/lowering/src/lower/exp/do_block.rs
@@ -1,4 +1,5 @@
 use polarity_lang_ast as ast;
+use polarity_lang_ast::ctx::BindContext;
 use polarity_lang_miette_util::ToMiette;
 use polarity_lang_parser::cst;
 
@@ -11,52 +12,65 @@ impl Lower for cst::exp::DoBlock {
     fn lower(&self, ctx: &mut Ctx) -> LoweringResult<Self::Target> {
         let cst::exp::DoBlock { span, statements } = self;
 
-        let mut statements = statements.iter().rev();
-
-        // The last element must be the final return expression.
-        let Some(cst::exp::DoStatement::Exp { span: ret_span, exp: ret_exp }) = statements.next()
-        else {
-            // This is impossible because the parser's grammar ensures that the last statement must
-            // always be the return expression.
+        let Some((last, statements)) = statements.split_last() else {
+            // The parser's grammar ensures there is at least one statement in the do block.
             return Err(Box::new(LoweringError::Impossible {
                 message: "No final return expression in do block.".to_string(),
                 span: Some(span.to_miette()),
             }));
         };
 
-        // Build the statement list from bottom to top.
-        let mut builder = ast::DoStatements::Return { span: *ret_span, exp: ret_exp.lower(ctx)? };
-        for statement in statements {
-            match statement {
-                cst::exp::DoStatement::Exp { span, exp } => {
-                    builder = ast::DoStatements::Bind {
-                        span: *span,
-                        name: ast::VarBind::Wildcard { span: None },
-                        bound: exp.lower(ctx)?,
-                        body: Box::new(builder),
-                    }
-                }
-                cst::exp::DoStatement::Bind { span, name, bound } => {
-                    builder = ast::DoStatements::Bind {
-                        span: *span,
-                        name: name.lower(ctx)?,
-                        bound: bound.lower(ctx)?,
-                        body: Box::new(builder),
-                    }
-                }
-                cst::exp::DoStatement::Let { span, name, typ, bound } => {
-                    builder = ast::DoStatements::Let {
-                        span: *span,
-                        name: name.lower(ctx)?,
-                        typ: typ.lower(ctx)?,
-                        bound: bound.lower(ctx)?,
-                        body: Box::new(builder),
-                    }
-                }
-            }
-        }
+        // The last element must be the final return expression.
+        let cst::exp::DoStatement::Exp { span: ret_span, exp: ret_exp } = last else {
+            // The parser's grammar ensures that the final statement is an expression.
+            return Err(Box::new(LoweringError::Impossible {
+                message: "Final statement in do block is not an expression.".to_string(),
+                span: Some(span.to_miette()),
+            }));
+        };
+        let return_exp = ast::DoStatements::Return { span: *ret_span, exp: ret_exp.lower(ctx)? };
 
-        let block = ast::DoBlock { span: *span, statements: builder, inferred_type: None };
+        let statements = lower_do_statements(statements, return_exp, ctx)?;
+
+        let block = ast::DoBlock { span: *span, statements, inferred_type: None };
         Ok(ast::Exp::DoBlock(block))
+    }
+}
+
+fn lower_do_statements(
+    statements: &[cst::exp::DoStatement],
+    return_exp: ast::DoStatements,
+    ctx: &mut Ctx,
+) -> LoweringResult<ast::DoStatements> {
+    let Some((head, tail)) = statements.split_first() else {
+        return Ok(return_exp);
+    };
+
+    match head {
+        cst::exp::DoStatement::Exp { span, exp } => Ok(ast::DoStatements::Bind {
+            span: *span,
+            name: ast::VarBind::Wildcard { span: None },
+            bound: exp.lower(ctx)?,
+            body: Box::new(lower_do_statements(tail, return_exp, ctx)?),
+        }),
+        cst::exp::DoStatement::Bind { span, name, bound } => {
+            let name = name.lower(ctx)?;
+            let bound = bound.lower(ctx)?;
+
+            ctx.bind_single(name.clone(), |ctx| {
+                let body = lower_do_statements(tail, return_exp, ctx)?;
+                Ok(ast::DoStatements::Bind { span: *span, name, bound, body: Box::new(body) })
+            })
+        }
+        cst::exp::DoStatement::Let { span, name, typ, bound } => {
+            let name = name.lower(ctx)?;
+            let typ = typ.lower(ctx)?;
+            let bound = bound.lower(ctx)?;
+
+            ctx.bind_single(name.clone(), |ctx| {
+                let body = lower_do_statements(tail, return_exp, ctx)?;
+                Ok(ast::DoStatements::Let { span: *span, name, typ, bound, body: Box::new(body) })
+            })
+        }
     }
 }
