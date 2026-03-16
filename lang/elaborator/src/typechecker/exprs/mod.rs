02610d9df01cd3297fc68d0df6b63a9eaa37585e
Rasmus Buurman <rasmus@rbuurman.de>
2026-03-16 21:05:30 +0100

    Typechecking of "do blocks" (#646)
    * wip: start typechecking do blocks
    
    * Typechecking of do statements
    
    * Fix typechecking of return expression
    
    * use imported name
    
    * Remove useless reference
    
    * Generate errors for invalid IO types
    
    * Do not erase do blocks
    
    * Added is_bin_op field accordingly
    
    * Improve error messages from ExpectIo
    
    * Add some test cases
    
    * Dont add bound value to context for binds
    
    * Use spaces in test files

--- lang/elaborator/src/typechecker/exprs/mod.rs
+++ lang/elaborator/src/typechecker/exprs/mod.rs
@@ -1,5 +1,6 @@
 pub mod anno;
 pub mod call;
+pub mod do_block;
 pub mod dot_call;
 pub mod hole;
 pub mod literal;
@@ -91,7 +92,7 @@ impl CheckInfer for Exp {
             Exp::LocalMatch(e) => Ok(e.check(ctx, t)?.into()),
             Exp::LocalComatch(e) => Ok(e.check(ctx, t)?.into()),
             Exp::LocalLet(e) => Ok(e.check(ctx, t)?.into()),
-            Exp::DoBlock(_) => todo!(),
+            Exp::DoBlock(e) => Ok(e.check(ctx, t)?.into()),
             Exp::Literal(e) => Ok(e.check(ctx, t)?.into()),
         }
     }
@@ -108,7 +109,7 @@ impl CheckInfer for Exp {
             Exp::LocalMatch(e) => Ok(e.infer(ctx)?.into()),
             Exp::LocalComatch(e) => Ok(e.infer(ctx)?.into()),
             Exp::LocalLet(e) => Ok(e.infer(ctx)?.into()),
-            Exp::DoBlock(_) => todo!(),
+            Exp::DoBlock(e) => Ok(e.infer(ctx)?.into()),
             Exp::Literal(e) => Ok(e.infer(ctx)?.into()),
         };
         trace!(
