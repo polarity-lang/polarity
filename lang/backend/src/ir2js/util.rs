82266669f5ba98695b2a65dc27d96aad26d2e228
Rasmus Buurman <rasmus@rbuurman.de>
2026-03-27 14:06:15 +0100

    Add some comments

--- lang/backend/src/ir2js/util.rs
+++ lang/backend/src/ir2js/util.rs
@@ -1,10 +1,12 @@
 use swc_common::{DUMMY_SP, SyntaxContext};
 use swc_ecma_ast as js;
 
+/// Wrap expression in parentheses.
 pub fn paren_expr(e: js::Expr) -> js::Expr {
     js::Expr::Paren(js::ParenExpr { span: DUMMY_SP, expr: Box::new(e) })
 }
 
+/// Force expression by directly calling it.
 pub fn force_expr(e: js::Expr) -> js::Expr {
     js::Expr::Call(js::CallExpr {
         span: DUMMY_SP,
