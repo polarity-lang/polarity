42f3e2f48e993c6c1497bbdd88ad4725556cce2d
Rasmus Buurman <rasmus@rbuurman.de>
2026-07-24 14:42:35 +0200

    Prefix extern calls in JS (#697)
    * Never rename extern calls
    
    * Prefix extern calls in JS

--- lang/backend/runtime/dist/runtime.js
+++ lang/backend/runtime/dist/runtime.js
@@ -1,41 +1,41 @@
 "use strict";
 const __fs = require("node:fs");
-function add_i64(x, y) {
+function $add_i64(x, y) {
     return BigInt.asIntN(64, x + y);
 }
-function sub_i64(x, y) {
+function $sub_i64(x, y) {
     return BigInt.asIntN(64, x - y);
 }
-function mul_i64(x, y) {
+function $mul_i64(x, y) {
     return BigInt.asIntN(64, x * y);
 }
-function div_i64(x, y) {
+function $div_i64(x, y) {
     return BigInt.asIntN(64, x / y);
 }
-function add_f64(x, y) {
+function $add_f64(x, y) {
     return x + y;
 }
-function sub_f64(x, y) {
+function $sub_f64(x, y) {
     return x - y;
 }
-function mul_f64(x, y) {
+function $mul_f64(x, y) {
     return x * y;
 }
-function div_f64(x, y) {
+function $div_f64(x, y) {
     return x / y;
 }
-function concat(s1, s2) {
+function $concat(s1, s2) {
     return s1.concat(s2);
 }
-function append_char(c, s) {
+function $append_char(c, s) {
     return s.concat(String.fromCodePoint(c));
 }
-function return_io(x) {
+function $return_io(x) {
     return function () {
         return x;
     };
 }
-function print(s) {
+function $print(s) {
     return function () {
         process.stdout.write(s);
         return {
@@ -44,7 +44,7 @@ function print(s) {
         };
     };
 }
-function read_file(path) {
+function $read_file(path) {
     return function () {
         try {
             const data = __fs.readFileSync(path, "utf8");
@@ -61,7 +61,7 @@ function read_file(path) {
         }
     };
 }
-function args() {
+function $args() {
     return function () {
         let args = {
             tag: "Nil",
