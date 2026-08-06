b5ebe8a355234ce82260bf8c55054a9d48821ebc
Rasmus Buurman <rasmus@rbuurman.de>
2026-08-06 23:58:49 +0200

    Add equality operators on primitives (#700)

--- lang/backend/runtime/runtime.ts
+++ lang/backend/runtime/runtime.ts
@@ -8,6 +8,9 @@ type Char = number;
 
 // data
 type Unit = { tag: "MkUnit"; args: [] };
+type Bool = { tag: "T"; args: [] } | { tag: "F"; args: [] };
+const __true: Bool = { tag: "T", args: [] };
+const __false: Bool = { tag: "F", args: [] };
 type Option<T> = { tag: "Some"; args: [T] } | { tag: "None"; args: [] };
 type List<T> = { tag: "Cons"; args: [T, List<T>] } | { tag: "Nil"; args: [] };
 function __arr_to_list<T>(arr: T[]): List<T> {
@@ -37,6 +40,10 @@ function $div_i64(x: I64, y: I64): I64 {
   return BigInt.asIntN(64, x / y);
 }
 
+function $eq_i64(x: I64, y: I64): Bool {
+  return x === y ? __true : __false;
+}
+
 function $add_f64(x: F64, y: F64): F64 {
   return x + y;
 }
@@ -53,6 +60,14 @@ function $div_f64(x: F64, y: F64): F64 {
   return x / y;
 }
 
+function $eq_f64(x: F64, y: F64): Bool {
+  return x === y ? __true : __false;
+}
+
+function $eq_char(x: Char, y: Char): Bool {
+  return x === y ? __true : __false;
+}
+
 function $concat_string(s1: String_, s2: String_): String_ {
   return s1.concat(s2);
 }
@@ -65,14 +80,18 @@ function $string_to_chars(s: String_): List<Char> {
   return __arr_to_list(Array.from(s, (c) => c.codePointAt(0)!));
 }
 
+function $eq_string(x: String_, y: String_): Bool {
+  return x === y ? __true : __false;
+}
+
 function $return_io<T>(x: T): IO<T> {
-  return async function() {
+  return async function () {
     return x;
   };
 }
 
 function $print(s: String_): IO<Unit> {
-  return async function() {
+  return async function () {
     process.stdout.write(s);
     return {
       tag: "MkUnit",
@@ -82,7 +101,7 @@ function $print(s: String_): IO<Unit> {
 }
 
 function $read_file(path: String_): IO<Option<String_>> {
-  return async function() {
+  return async function () {
     try {
       const data: String_ = __fs.readFileSync(path, "utf8");
       return {
@@ -99,7 +118,7 @@ function $read_file(path: String_): IO<Option<String_>> {
 }
 
 function $args(): IO<List<String_>> {
-  return async function() {
+  return async function () {
     return __arr_to_list(process.argv.slice(2));
   };
 }
